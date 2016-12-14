bb845242b69c46d4f3615f536a88762711958688
Binimvarghese <binivarghese@expressbase.com>
2016-12-14 11:07:40 +0530

    ExpressBase.Web updated

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpressBase.Web2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}

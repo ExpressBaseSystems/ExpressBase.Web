6a95d984349654318a71de04ba8db7002850c705
RobyAbraham <robyabraham@expressbase.com>
2017-04-05 15:53:39 +0530

    new js function added etc.

--- wwwroot/js/eb_common.js
+++ wwwroot/js/eb_common.js
@@ -6,3 +6,26 @@ function getCookieVal(a) {
     var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
     return b ? b.pop() : '';
 }
+
+function fltr_obj(type, name, value) {
+    this.type = type;
+    this.name = name;
+    this.value = value;
+}
+
+function getFilterValues() {
+    var fltr_collection = [];
+    var params = [] ;
+    params = $('#hiddenparams').val().split(',');
+    $.each(params, function (i, id) {
+        var v;
+        var dtype = $('#' + id).attr('data-type');
+        if(dtype === 'datetime') {
+            v = $('#' + id).val().substring(0, 10);
+        }
+        else 
+            v = $('#' + id).val();
+        fltr_collection.push(new fltr_obj(dtype, id, v));
+    });
+    return fltr_collection;
+}

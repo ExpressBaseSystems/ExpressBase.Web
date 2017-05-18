9a812f89b9d7b697214b1991fc0c2375e9687832
Jith Job K.I <jithjob@expressbase.com>
2017-05-18 23:55:54 +0530

    Ebselect objectization 3

--- wwwroot/js/eb_common.js
+++ wwwroot/js/eb_common.js
@@ -50,3 +50,17 @@ Array.prototype.contains = function (element) {
 
 //    return arr;
 //};
+
+function isPrintable(e) {
+    var keycode = e.keyCode;
+
+    var valid = 
+        (keycode > 47 && keycode < 58)   || // number keys
+        keycode == 32 || keycode == 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+        (keycode > 64 && keycode < 91)   || // letter keys
+        (keycode > 95 && keycode < 112)  || // numpad keys
+        (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+        (keycode > 218 && keycode < 223);   // [\]' (in order)
+
+    return valid;
+}

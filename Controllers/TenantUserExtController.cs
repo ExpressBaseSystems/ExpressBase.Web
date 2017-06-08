df3e85308906c53b81890302273400a105f93de9
RobyAbraham <robyabraham@expressbase.com>
2017-06-08 11:11:18 +0530

    Merged commit...

--- Controllers/TenantUserExtController.cs
+++ Controllers/TenantUserExtController.cs
@@ -32,12 +32,107 @@ namespace ExpressBase.Web2.Controllers
             return View();
         }
       
-        public IActionResult TenantUserLogin(int i)
+        public async Task<IActionResult> TenantUserLogin(int i)
         {
+            ViewBag.EbConfig = this.EbConfig;
+
             var req = this.HttpContext.Request.Form;
             ViewBag.cid = req["cid"];
-            ViewBag.EbConfig = this.EbConfig;
-            return View();
+            MyAuthenticateResponse authResponse = null;
+
+            string token = req["g-recaptcha-response"];
+            Recaptcha data = await RecaptchaResponse("6Lf3UxwUAAAAACIoZP76iHFxb-LVNEtj71FU2Vne", token);
+            if (!data.Success)
+            {
+                if (data.ErrorCodes.Count <= 0)
+                {
+                    return View();
+                }
+                var error = data.ErrorCodes[0].ToLower();
+                switch (error)
+                {
+                    case ("missing-input-secret"):
+                        ViewBag.CaptchaMessage = "The secret parameter is missing.";
+                        break;
+                    case ("invalid-input-secret"):
+                        ViewBag.CaptchaMessage = "The secret parameter is invalid or malformed.";
+                        break;
+
+                    case ("missing-input-response"):
+                        ViewBag.CaptchaMessage = "The captcha input is missing.";
+                        break;
+                    case ("invalid-input-response"):
+                        ViewBag.CaptchaMessage = "The captcha input is invalid or malformed.";
+                        break;
+
+                    default:
+                        ViewBag.CaptchaMessage = "Error occured. Please try again";
+                        break;
+                }
+                return View();
+            }
+            else
+            {
+                try
+                {
+                    var authClient = this.EbConfig.GetServiceStackClient();
+                    authResponse = authClient.Send<MyAuthenticateResponse>(new Authenticate
+                    {
+                        provider = MyJwtAuthProvider.Name,
+                        UserName = req["uname"],
+                        Password = req["pass"],
+                        Meta = new Dictionary<string, string> { { "cid", req["cid"] }, { "Login", "Client" } },
+                        UseTokenCookie = true
+                    });
+
+                }
+                catch (WebServiceException wse)
+                {
+                    ViewBag.errormsg = wse.Message;
+                    return View();
+                }
+                catch (Exception wse)
+                {
+                    ViewBag.errormsg = wse.Message;
+                    return View();
+                }
+
+                if (authResponse != null && authResponse.ResponseStatus != null
+                    && authResponse.ResponseStatus.ErrorCode == "EbUnauthorized")
+                {
+                    ViewBag.errormsg = "Please enter a valid Username/Password";
+                    return View("TenantUserLogin");
+                }
+
+                RoleCollection RoleCollection = authResponse.User.RoleCollection;
+                CookieOptions options = new CookieOptions();
+
+                if (RoleCollection.HasSystemRole())
+                {
+                    Response.Cookies.Append(string.Format("T_{0}", ViewBag.cid), authResponse.BearerToken, options);
+                    Response.Cookies.Append("cid", ViewBag.cid);
+
+                    if (req.ContainsKey("remember"))
+                    {
+                        Response.Cookies.Append("UserName", req["uname"], options);
+                        Response.Cookies.Append("UId", authResponse.UserId, options);
+                    }
+                    return RedirectToAction("DevConsole", new RouteValueDictionary(new { controller = "Tenant", action = "DevConsole", id = authResponse.UserId }));
+                }
+                else
+                {
+
+                    Response.Cookies.Append(string.Format("T_{0}", ViewBag.cid), authResponse.BearerToken, options);
+
+                    if (req.ContainsKey("remember"))
+                    {
+                        Response.Cookies.Append("UserName", req["uname"], options);
+                        Response.Cookies.Append("UId", authResponse.UserId, options);
+                    }
+
+                    return RedirectToAction("UserDashboard", new RouteValueDictionary(new { controller = "TenantUser", action = "UserDashboard", id = authResponse.UserId }));
+                }
+            }
         }
        
         [HttpGet]
@@ -48,6 +143,8 @@ namespace ExpressBase.Web2.Controllers
             ViewBag.cookie = Request.Cookies["UserName"];
             ViewBag.Userid = Request.Cookies["UId"];
             ViewBag.cid = cid;
+            CookieOptions options = new CookieOptions();
+            Response.Cookies.Append("cid", cid, options);
             if (!string.IsNullOrEmpty(ViewBag.cookie))
             {
                 var redisClient = EbConfig.GetRedisClient();
@@ -58,7 +155,7 @@ namespace ExpressBase.Web2.Controllers
         }
 
 
-        //[Microsoft.AspNetCore.Mvc.Route("{cid}")]
+        [Microsoft.AspNetCore.Mvc.Route("{cid}")]
         [HttpPost]
         public async Task<IActionResult> TenantUserLogin()
         {
@@ -135,21 +232,7 @@ namespace ExpressBase.Web2.Controllers
                 RoleCollection RoleCollection = authResponse.User.RoleCollection;
                 CookieOptions options = new CookieOptions();
 
-                if (RoleCollection.HasSystemRole())
-                {
-                    Response.Cookies.Append(string.Format("T_{0}", ViewBag.cid), authResponse.BearerToken, options);
-                    Response.Cookies.Append("cid", ViewBag.cid);
-
-                    if (req.ContainsKey("remember"))
-                    {
-                        Response.Cookies.Append("UserName", req["uname"], options);
-                        Response.Cookies.Append("UId", authResponse.UserId, options);
-                    }
-                    return RedirectToAction("DevConsole", new RouteValueDictionary(new { controller = "Tenant", action = "DevConsole", id = authResponse.UserId }));
-                }
-                else
-                {
-
+               
                     Response.Cookies.Append(string.Format("T_{0}", ViewBag.cid), authResponse.BearerToken, options);
 
                     if (req.ContainsKey("remember"))
@@ -159,7 +242,7 @@ namespace ExpressBase.Web2.Controllers
                     }
 
                     return RedirectToAction("UserDashboard", new RouteValueDictionary(new { controller = "TenantUser", action = "UserDashboard", id = authResponse.UserId }));
-                }
+                
             }
         }
 
--- Filters/BaseController.cs
+++ Filters/BaseController.cs
@@ -26,15 +26,18 @@ namespace ExpressBase.Web.Filters
             var controller = context.Controller as Controller;
             var handler = new JwtSecurityTokenHandler();
             var token = context.HttpContext.Request.Cookies["Token"];
-            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
-            controller.ViewBag.Fname = tokenS.Claims.First(claim => claim.Type == "Fname").Value;
-            controller.ViewBag.tier = context.HttpContext.Request.Query["tier"];
-            controller.ViewBag.tenantid = context.HttpContext.Request.Query["id"];
-            controller.ViewBag.token = token;
-            controller.ViewBag.UId = Convert.ToInt32(tokenS.Claims.First(claim => claim.Type == "uid").Value);
-            controller.ViewBag.cid = tokenS.Claims.First(claim => claim.Type == "cid").Value;
             controller.ViewBag.EbConfig = this.EbConfig;
-           
+            if (!string.IsNullOrEmpty(token))
+            {
+                var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+                controller.ViewBag.Fname = tokenS.Claims.First(claim => claim.Type == "Fname").Value;
+                controller.ViewBag.tier = context.HttpContext.Request.Query["tier"];
+                controller.ViewBag.tenantid = context.HttpContext.Request.Query["id"];
+                controller.ViewBag.token = token;
+                controller.ViewBag.UId = Convert.ToInt32(tokenS.Claims.First(claim => claim.Type == "uid").Value);
+                controller.ViewBag.cid = tokenS.Claims.First(claim => claim.Type == "cid").Value;
+               
+            }
 
             if (!string.IsNullOrEmpty(TAccountId))
             {

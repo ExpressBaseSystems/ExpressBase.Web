cd02b3c527405e0e48d812d0f5146809dfa074af
DonajoseUllattil <donajose@expressbase.com>
2017-07-13 13:21:30 +0530

    minor

--- Controllers/DevController.cs
+++ Controllers/DevController.cs
@@ -74,7 +74,7 @@ namespace ExpressBase.Web.Controllers
 
             ViewBag.Obj_id = obj_id;
             IServiceClient client = this.EbConfig.GetServiceStackClient(ViewBag.token, ViewBag.rToken);
-            var resultlist = client.Get<EbObjectResponse>(new EbObjectRequest { Id = obj_id, VersionId = Int32.MaxValue, EbObjectType = (int) EbObjectType.DataSource, Token = ViewBag.token });
+            var resultlist = client.Get<EbObjectResponse>(new EbObjectRequest { Id = obj_id, VersionId = -1, EbObjectType = (int) EbObjectType.DataSource, Token = ViewBag.token });
             var rlist = resultlist.Data;
             foreach (var element in rlist)
             {
@@ -115,8 +115,7 @@ namespace ExpressBase.Web.Controllers
             foreach (var element in fdrlist)
             {
                 filterDialogs[element.Id] = element;
-            }
-            ViewBag.FilterDialogId = "null";//related to 
+            }         
             return filterDialogs;
         }
 
@@ -212,16 +211,16 @@ namespace ExpressBase.Web.Controllers
         //for ajax call
 
         [HttpPost]
-        public string VersionCodes()
+        public string VersionCodes(int objid)
         {
-            var req = this.HttpContext.Request.Form;
-            var objid = Convert.ToInt32(req["objid"]);
-            var vers_id = Convert.ToInt32(req["vers_id"]);
-          var obj_type = (EbObjectType)Convert.ToInt32(req["obj_type"]);
+          //  var req = this.HttpContext.Request.Form;
+          //  var objid = Convert.ToInt32(req["objid"]);
+          //  var vers_id = Convert.ToInt32(req["vers_id"]);
+          //var obj_type = (EbObjectType)Convert.ToInt32(req["obj_type"]);
             //EbObjectType content;
             //Enum.TryParse(req["obj_type"], out content);
             IServiceClient client = this.EbConfig.GetServiceStackClient(ViewBag.token, ViewBag.rToken);
-            var resultlist = client.Get<EbObjectResponse>(new EbObjectRequest { Id = objid, VersionId = objid, EbObjectType = (int)obj_type, Token = ViewBag.token });
+            var resultlist = client.Get<EbObjectResponse>(new EbObjectRequest { Id = objid, VersionId = objid, EbObjectType = (int)EbObjectType.DataSource, Token = ViewBag.token });
             var rlist = resultlist.Data;
             foreach (var element in rlist)
             {

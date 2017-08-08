1659906ec3adf9cfeb61262e430c0c28cc8d08e2
RobyAbraham <robyabraham@expressbase.com>
2017-08-08 13:29:32 -0700

    RefId related changes

---
+++ Components/ParameterDivViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using ExpressBase.Objects.ObjectContainers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Components
+{
+    public class ParameterDivViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(EbFilterDialog paramDiv)
+        {
+            ViewBag.HtmlHead = paramDiv.GetHead();
+            ViewBag.HtmlBody = paramDiv.GetHtml();
+
+            return View();
+        }
+    }
+}

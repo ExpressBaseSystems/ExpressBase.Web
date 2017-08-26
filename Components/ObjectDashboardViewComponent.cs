ccb4310206d777f09686aea008ef138d7575b529
DonajoseUllattil <donajose@expressbase.com>
2017-08-26 11:59:43 +0530

    work on object dashboard

--- Components/ObjectDashboardViewComponent.cs
+++ Components/ObjectDashboardViewComponent.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ExpressBase.Common.Objects;
 
 namespace ExpressBase.Web.Components
 {
     public class ObjectDashboardViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string objid,string objname,int totVer,ObjectLifeCycleStatus status)
         {
+            ViewBag.ObjName = objname;
+            ViewBag.TotalVersions= totVer;
+            ViewBag.Status = status;
              return View();
 
         }

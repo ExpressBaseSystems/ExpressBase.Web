4bea8c0d4d614841dbc7b77c665183119a26ed07
Jith Job K.I <jithjob@expressbase.com>
2017-08-28 19:05:20 +0530

    slideFn optimized

--- wwwroot/js/Eb_UtilityFuncions.js
+++ wwwroot/js/Eb_UtilityFuncions.js
@@ -1,40 +1,20 @@
 ﻿function slideRight(leftDiv, rightDiv) {
+    $stickBtn = $("<div id='stickBtnR' class='stickBtn' style='right: 0px;' onclick=\"slideRight('" + leftDiv + "', '" + rightDiv + "')\">PropertyBox</div>");
+    slide("right", leftDiv, rightDiv, $stickBtn);
+};
+
+function slideLeft(leftDiv, rightDiv) {
+    $stickBtn = $("<div id='stickBtnL' class='stickBtn' onclick=\"slideLeft('" + leftDiv + "', '" + rightDiv + "')\">ToolBox</div>");
+    slide("left", leftDiv, rightDiv, $stickBtn);
+};
+
+function slide(dir, leftDiv, rightDiv, $stickBtn) {
     var $leftDiv = $(leftDiv);
     var $rightDiv = $(rightDiv);
-
-    $stickBtn = $("<div id='stickBtnR' class='stickBtn' style='right: 0px;' onclick=\"slideRight('" + leftDiv + "', '" + rightDiv + "')\">PropertyBox</div>");
 
     lW = parseFloat($leftDiv.css("width"));
     rW = parseFloat($rightDiv.css("width"));
 
-    if ($rightDiv.css("display") === "inline-block") {
-        $rightDiv.animate({ width: 0 }, 300);
-        $leftDiv.animate({ width: lW + rW + "px" }, 300);
-
-        setTimeout(function () {
-            $(".form-buider-cont").append($stickBtn);
-            $stickBtn.css({ "right": (0 - ($stickBtn.width() / 2)) + "px", "top": (198 + ($stickBtn.width() / 2)) });
-            $rightDiv.data("width", rW);
-            $rightDiv.hide();
-        }, 301);
-    }
-    else {
-        rW = $rightDiv.data("width");
-        $("#stickBtnR").remove();
-        $rightDiv.show();
-        $rightDiv.animate({ width: rW + "px" }, 300);
-        $leftDiv.animate({ width: (lW - rW) + "px" }, 300);
-    }
-};
-
-function slideLeft(leftDiv, rightDiv) {
-    var $leftDiv = $(leftDiv);
-    var $rightDiv = $(rightDiv);
-
-    $stickBtn = $("<div id='stickBtnL' class='stickBtn' onclick=\"slideLeft('" + leftDiv + "', '" + rightDiv + "')\">ToolBox</div>");
-
-    lW = parseFloat($leftDiv.css("width"));
-    rW = parseFloat( $rightDiv.css("width"));
     if ($rightDiv.css("display") === "inline-block") {
         $rightDiv.data("width", rW);
         $rightDiv.animate({ width: 0 }, 300);
@@ -42,16 +22,20 @@ function slideLeft(leftDiv, rightDiv) {
 
         setTimeout(function () {
             $(document.body).append($stickBtn);
-            $stickBtn.css({ "left": (0 - ($stickBtn.width() / 2)) + "px", "top": (198 + ($stickBtn.width() / 2)) });
+            $stickBtn.css({ dir: (0 - ($stickBtn.width() / 2)) + "px", "top": (198 + ($stickBtn.width() / 2)) });
             $rightDiv.hide();
         }, 301);
     }
     else {
         rW = $rightDiv.data("width");
-        $("#stickBtnL").remove();
+        if (dir === "right")
+            $("#stickBtnR").remove();
+        else
+            $("#stickBtnL").remove();
+
         $rightDiv.show();
         $rightDiv.animate({ width: rW + "px" }, 300);
-        $leftDiv.animate({ width: (lW - rW ) + "px" }, 300);
+        $leftDiv.animate({ width: (lW - rW) + "px" }, 300);
     }
 };
 

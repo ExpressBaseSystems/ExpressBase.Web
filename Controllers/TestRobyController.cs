9fddf95272b6d946195ab0d5b8d054f72e70a4f3
RobyAbraham <robyabraham@expressbase.com>
2017-09-12 18:48:57 +0530

    test map

---
+++ Controllers/TestRobyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExpressBase.Web.Controllers
+{
+    public class TestRobyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult map()
+        {
+            return View();
+        }
+    }
+}

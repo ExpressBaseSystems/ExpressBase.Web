9fe18e792100fed880748ace5f49ffda5d98a517
Jith Job K.I <jithjob@expressbase.com>
2017-09-13 19:08:15 +0530

    pg OSE versionSelect added

--- Controllers/DVController.cs
+++ Controllers/DVController.cs
@@ -355,15 +355,11 @@ namespace ExpressBase.Web.Controllers
         }
 
         // Get All DVNames for Linking with each other -href click
-        public Dictionary<string, string> FetchAllDataVisualizations(EbObjectType type)
+        public Dictionary<string, List<EbObjectWrapper>> FetchAllDataVisualizations(EbObjectType type)
         {
-            var resultlist = this.ServiceClient.Get<EbObjectObjListResponse>(new EbObjectObjListRequest {  EbObjectType = (int)type});
+            var resultlist = this.ServiceClient.Get<EbObjectObjListAllVerResponse>(new EbObjectObjLisAllVerRequest {  EbObjectType = Convert.ToInt32(type) });
+            var ObjDVListAll = resultlist.Data;
 
-            Dictionary<string, string> ObjDVListAll = new Dictionary<string, string>();
-            foreach (var element in resultlist.Data)
-                ObjDVListAll[element.RefId] = element.Name;
-
-            //ViewBag.DVListAll = ObjDVListAll;
             return ObjDVListAll;
         }
      }

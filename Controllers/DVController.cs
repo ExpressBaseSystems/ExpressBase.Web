6da418b58a8129300232254b7a26c6fe62be1b32
RobyAbraham <robyabraham@expressbase.com>
2017-09-19 01:52:02 +0530

    dv related changes..

--- Controllers/DVController.cs
+++ Controllers/DVController.cs
@@ -168,9 +168,9 @@ namespace ExpressBase.Web.Controllers
         //    return View();
         //}
         
-        public IActionResult dvCommon(string dsRefId, string Meta, string dvRefId)
+        public IActionResult dvCommon(List<EbDataVisualization> dvset, string Meta, string dvRefId)
         {
-            return ViewComponent("DataVisualization", new { dsRefid = dsRefId, Meta = Meta , dvRefId = dvRefId });
+            return ViewComponent("DataVisualization", new { dvset = dvset, Meta = Meta , dvRefId = dvRefId });
         }
 
         //public PartialViewResult DataVisualisation(int dsid)

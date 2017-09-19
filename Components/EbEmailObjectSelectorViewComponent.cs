4c22a0b98e28046f02208e5a645d4f2d620eaee3
Bini Varghese <binivarghese@expressbase.com>
2017-09-19 18:13:41 +0530

    for emailbuilder

---
+++ Components/EbEmailObjectSelectorViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Components
+{
+    public class EbEmailObjectSelectorViewComponent : ViewComponent
+    {
+
+        protected JsonServiceClient ServiceClient { get; set; }
+
+        protected IRedisClient redis { get; set; }
+
+        public EbEmailObjectSelectorViewComponent(IServiceClient _client, IRedisClient _redis)
+        {
+            this.ServiceClient = _client as JsonServiceClient;
+            this.redis = _redis;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int Type)
+        {
+            var resultlist = this.ServiceClient.Get<EbObjectObjListResponse>(new EbObjectObjListRequest { EbObjectType = Type });
+            var rlist = resultlist.Data;
+            Dictionary<int, EbObjectWrapper> ObjList = new Dictionary<int, EbObjectWrapper>();
+            foreach (var element in rlist)
+            {
+                ObjList[element.Id] = element;
+            }
+            ViewBag.Objlist = ObjList;
+
+            return View();
+        }
+    }
+}

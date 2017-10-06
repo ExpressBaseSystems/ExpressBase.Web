347a766b1b885894a69e1aae119a4210f80698f2
RobyAbraham <robyabraham@expressbase.com>
2017-10-06 19:38:06 +0530

    dv multi view related..

--- wwwroot/js/dvContainer.js
+++ wwwroot/js/dvContainer.js
@@ -1,4 +1,5 @@
 ﻿var counter = 0;
+var focusedId;
 var DvContainerObj = function (settings) {
     this.ssurl = settings.ss_url;
     this.wc = settings.wc;
@@ -8,6 +9,7 @@ var DvContainerObj = function (settings) {
     this.UniqueId = null;
     this.ebdtable = {};
     this.chartJs = {};
+    this.previousObj = null;
 
     this.init = function () {
         $("#btnGo").off("click").on("click", this.btnGoClick.bind(this));
@@ -17,44 +19,52 @@ var DvContainerObj = function (settings) {
         $("#last").off("click").on("click", this.gotoLast.bind(this));
         $("#Save_btn").off("click").on("click", this.saveSettings.bind(this));
         //$("#Related" + this.tableId + " .dropdown-menu li a").off("click").on("click", this.drawDv.bind(this));
+        $("#fd_toggle").off("click").on("click", this.ToggleParamDiv.bind(this));
     };
 
 
     this.btnGoClick = function () {
+        $("#fd_toggle").css("display", "inline");
         this.UniqueId = "dv" + this.currentObj.EbSid + "_" + counter;
-        console.log(this.dvcol["sub_window_dv" + this.currentObj.EbSid + "_" + counter]);
+        console.log(this.dvcol[focusedId]);
         if (this.currentObj.$type.indexOf("EbTableVisualization") !== -1) {
-            this.ebdtable[this.UniqueId] = new EbDataTable({
+            this.MainData = (dvcontainerObj.currentObj.Pippedfrom !== null) ? dvcontainerObj.previousObj.data : null;
+            this.ebdtable[focusedId] = new EbDataTable({
                 ds_id: this.currentObj.DataSourceRefId,
                 ss_url: this.ssurl,
                 tid: this.UniqueId,
                 login: this.wc,
                 settings: this.currentObj,
+                data: this.MainData,
             });
 
-            this.ebdtable[this.UniqueId].getColumnsSuccess(this.currentObj);
+            this.ebdtable[focusedId].getColumnsSuccess(this.currentObj);
         }
         else if (this.currentObj.$type.indexOf("EbChartVisualization") !== -1) {
             //this.UniqueId = "dv" + this.currentObj.EbSid + "_" + counter;
-            this.chartJs[this.UniqueId] = new eb_chart(this.currentObj, this.ssurl, this.MainData, this.UniqueId);
+            this.MainData = (dvcontainerObj.currentObj.Pippedfrom !== null) ? dvcontainerObj.previousObj.data : null;
+            this.chartJs[focusedId] = new eb_chart(this.currentObj, this.ssurl, this.MainData, this.UniqueId);
         }
-        console.log("xxxxx", this.dvcol["sub_window_dv" + this.currentObj.EbSid + "_" + counter]);
+        console.log("xxxxx", this.dvcol[focusedId]);
+        console.log("ccccc", dvcontainerObj.currentObj);
     };
 
     this.gotoNext = function () {
         focusedId = $("#" + focusedId).next().attr("id");
         $("#" + focusedId).focus();
         var dvobj = this.dvcol[focusedId];
+        dvcontainerObj.previousObj = dvcontainerObj.currentObj;
+        dvcontainerObj.currentObj = dvobj;
         if (dvobj.$type.indexOf("EbTableVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find(".dataTables_scroll").length > 0) {
-                this.ebdtable["dv" + dvobj.EbSid + "_" + counter].GenerateButtons();
+                this.ebdtable[focusedId].GenerateButtons();
             }
         }
         else if (dvobj.$type.indexOf("EbChartVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find("canvas").length > 0) {
-                this.chartJs["dv" + dvobj.EbSid + "_" + counter].createButtons();
+                this.chartJs[focusedId].createButtons();
             }
         }
         if ($("#" + focusedId).next().attr("id") == undefined) {
@@ -71,16 +81,18 @@ var DvContainerObj = function (settings) {
         focusedId = $("#" + focusedId).prev().attr("id");
         $("#" + focusedId).focus();
         var dvobj = this.dvcol[focusedId];
+        dvcontainerObj.previousObj = dvcontainerObj.currentObj;
+        dvcontainerObj.currentObj = dvobj;
         if (dvobj.$type.indexOf("EbTableVisualization") !== -1) {
             if ($("#" + focusedId).find(".dataTables_scroll").length > 0) {
                 $("#Toolbar").children(":not(.commonControls)").remove();
-                this.ebdtable["dv" + dvobj.EbSid + "_" + counter].GenerateButtons();
+                this.ebdtable[focusedId].GenerateButtons();
             }
         }
         else {
             if ($("#" + focusedId).find("canvas").length > 0) {
                 $("#Toolbar").children(":not(.commonControls)").remove();
-                this.chartJs["dv" + dvobj.EbSid + "_" + counter].createButtons();
+                this.chartJs[focusedId].createButtons();
             }
         }
         if ($("#" + focusedId).prev().attr("id") == undefined) {
@@ -97,16 +109,18 @@ var DvContainerObj = function (settings) {
         focusedId = $("#" + focusedId).siblings().first().attr("id");
         $("#" + focusedId).focus();
         var dvobj = this.dvcol[focusedId];
+        dvcontainerObj.previousObj = dvcontainerObj.currentObj;
+        dvcontainerObj.currentObj = dvobj;
         if (dvobj.$type.indexOf("EbTableVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find(".dataTables_scroll").length > 0) {
-                this.ebdtable["dv" + dvobj.EbSid + "_" + counter].GenerateButtons();
+                this.ebdtable[focusedId].GenerateButtons();
             }
         }
         else if (dvobj.$type.indexOf("EbChartVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find("canvas").length > 0) {
-                this.chartJs["dv" + dvobj.EbSid + "_" + counter].createButtons();
+                this.chartJs[focusedId].createButtons();
             }
         }
         if ($("#" + focusedId).prev().attr("id") == undefined) {
@@ -123,16 +137,18 @@ var DvContainerObj = function (settings) {
         focusedId = $("#" + focusedId).siblings().last().attr("id");
         $("#" + focusedId).focus();
         var dvobj = this.dvcol[focusedId];
+        dvcontainerObj.previousObj = dvcontainerObj.currentObj;
+        dvcontainerObj.currentObj = dvobj;
         if (dvobj.$type.indexOf("EbTableVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find(".dataTables_scroll").length > 0) {
-                this.ebdtable["dv" + dvobj.EbSid + "_" + counter].GenerateButtons();
+                this.ebdtable[focusedId].GenerateButtons();
             }
         }
         else if (dvobj.$type.indexOf("EbChartVisualization") !== -1) {
             $("#Toolbar").children(":not(.commonControls)").remove();
             if ($("#" + focusedId).find("canvas").length > 0) {
-                this.chartJs["dv" + dvobj.EbSid + "_" + counter].createButtons();
+                this.chartJs[focusedId].createButtons();
             }
         }
         if ($("#" + focusedId).next().attr("id") == undefined) {
@@ -145,17 +161,29 @@ var DvContainerObj = function (settings) {
         }
     };
 
-    //this.drawDv = function (e) {
-    //    $.LoadingOverlay("show");
-    //    $.ajax({
-    //        type: "POST",
-    //        url: "../DV/getdv",
-    //        data: { dvRefId: $(e.target).attr("data-refid") },
-    //        success: function (text) {
-    //                $.LoadingOverlay("hide");
-    //        }
-    //    });
-    //};
+    this.drawDv = function (e) {
+        $.LoadingOverlay("show");
+        $.ajax({
+            type: "POST",
+            url: "../DV/getdv",
+            data: { id: $(e.target).attr("data-id"), objtype: $(e.target).attr("objtype") },
+            success: function (dvObj) {
+                dvObj = JSON.parse(dvObj);
+                dvcontainerObj.currentObj = dvObj;
+                $.LoadingOverlay("hide");
+                if (dvObj.$type.indexOf("EbTableVisualization") !== -1) {
+                    pg.setObject(dvObj, AllMetas["EbTableVisualization"]);
+                    split.createContentWindow(dvObj.EbSid + "_" + ++counter, "EbTableVisualization");
+                    call2dvView(dvObj);
+                }
+                else if (dvObj.$type.indexOf("EbChartVisualization") !== -1) {
+                    pg.setObject(dvObj, AllMetas["EbChartVisualization"]);
+                    split.createContentWindow(dvObj.EbSid + "_" + ++counter, "EbChartVisualization");
+                    call2dvView(dvObj);
+                }
+            }
+        });
+    };
 
     this.saveSettings = function () {
         if (dvcontainerObj.currentObj.$type.indexOf("EbTableVisualization") !== -1)
@@ -167,6 +195,15 @@ var DvContainerObj = function (settings) {
     this.saveSuccess = function () {
         alert("Success!!!!!!!");
     }
+
+    this.ToggleParamDiv = function () {
+        $("#" + focusedId).children(".fd").toggle();
+        if ($("#" + focusedId).children(".fd").css("display") === "none")
+            $("#" + focusedId).children("div:not(.fd)").removeClass("col-md-10").addClass("col-md-12");
+        else
+            $("#" + focusedId).children("div:not(.fd)").removeClass("col-md-12").addClass("col-md-10");
+
+    };
 
     this.init();
 }

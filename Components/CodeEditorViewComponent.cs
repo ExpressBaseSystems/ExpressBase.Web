be18c97aa8f11ada3409116ca44a10f8f0b63f76
DonajoseUllattil <donajose@expressbase.com>
2017-10-23 18:39:05 +0530

    objects to viewcomponent

--- Components/CodeEditorViewComponent.cs
+++ Components/CodeEditorViewComponent.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpressBase.Common.Objects;
+using ExpressBase.Objects;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ExpressBase.Web.Components
 {
     public class CodeEditorViewComponent: ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(string dsobj, int tabnum, int type)
         {
-            return View();
+            ViewBag.dsObj = dsobj;
+            ViewBag.tabnum = tabnum;
+            ViewBag.ObjType = type;
 
+            var typeArray = typeof(EbDatasourceMain).GetTypeInfo().Assembly.GetTypes();
+            var _jsResult = CSharpToJs.GenerateJs<EbDatasourceMain>(BuilderType.DataSource, typeArray);
+            ViewBag.Meta = _jsResult.Meta;
+            ViewBag.JsObjects = _jsResult.JsObjects;
+            ViewBag.EbObjectTypes = _jsResult.EbObjectTypes;
+
+            return View();
         }
     }
 }
--- Controllers/Eb_ObjectController.cs
+++ Controllers/Eb_ObjectController.cs
@@ -8,6 +8,8 @@ using ServiceStack.Redis;
 using ExpressBase.Common;
 using ExpressBase.Objects.ServiceStack_Artifacts;
 using ExpressBase.Common.Objects;
+using ExpressBase.Objects;
+using System.Reflection;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,6 +20,43 @@ namespace ExpressBase.Web.Controllers
         public Eb_ObjectController(IServiceClient sclient, IRedisClient redis) : base(sclient, redis) { }
         public IActionResult Index()
         {
+            var req = this.HttpContext.Request.Form;
+            int obj_id = Convert.ToInt32(req["objid"]);
+
+            ViewBag.Obj_id = obj_id;
+            var resultlist = this.ServiceClient.Get<EbObjectExploreObjectResponse>(new EbObjectExploreObjectRequest { Id = obj_id });
+            var rlist = resultlist.Data;
+            foreach (var element in rlist)
+            {
+                ViewBag.IsNew = "false";
+                ViewBag.ObjectName = element.Name;
+                ViewBag.ObjectDesc = element.Description;
+                ViewBag.Status = element.Status;
+                ViewBag.VersionNumber = element.VersionNumber;
+                ViewBag.ObjType = (int)EbObjectType.DataSource;
+                ViewBag.Refid = element.RefId;
+                ViewBag.Majorv = element.MajorVersionNumber;
+                ViewBag.Minorv = element.MinorVersionNumber;
+                ViewBag.Patchv = element.PatchVersionNumber;
+                ViewBag.Tags = element.Tags;
+
+                if (String.IsNullOrEmpty(element.Json_wc) && !String.IsNullOrEmpty(element.Json_lc))
+                {
+                    ViewBag.ReadOnly = true;
+                    var dsobj = EbSerializers.Json_Deserialize(element.Json_lc);
+                    ViewBag.dsObj = dsobj;
+                    ViewBag.Workingcopy = element.Wc_All;
+                }
+                else if (String.IsNullOrEmpty(element.Json_lc) && !String.IsNullOrEmpty(element.Json_wc))
+                {
+                    ViewBag.ReadOnly = false;
+                    var dsobj = EbSerializers.Json_Deserialize(element.Json_wc);
+                    ViewBag.dsObj = dsobj;
+                    ViewBag.Workingcopy = element.Wc_All;
+                }
+            }
+            
+
             return View();
         }
         public string CommitEbObject(string _refid, string _json, string _changeLog, string _rel_obj, string _tags)
@@ -97,6 +136,35 @@ namespace ExpressBase.Web.Controllers
         public IActionResult VersionHistory(string objid, int tabNum)
         {
             return ViewComponent("VersionHistory", new {objid = objid, tabnum = tabNum });
+        }
+
+
+        [HttpPost]
+        public string VersionCodes(string objid, int objtype)
+        {
+            EbDataSource dsobj = null;
+            var _EbObjectType = (EbObjectType)objtype;
+            var resultlist = this.ServiceClient.Get<EbObjectParticularVersionResponse>(new EbObjectParticularVersionRequest { RefId = objid });
+            var rlist = resultlist.Data;
+            foreach (var element in rlist)
+            {
+                if (_EbObjectType == EbObjectType.DataSource)
+                {
+                    var obj = new EbObjectWrapper();
+                    dsobj = EbSerializers.Json_Deserialize(element.Json);
+                    ViewBag.Code = dsobj.Sql;
+                    dsobj.Status = element.Status;
+                    dsobj.VersionNumber = element.VersionNumber;
+                }
+            }
+            return EbSerializers.Json_Serialize(dsobj);
+        }
+
+
+        [HttpPost]
+        public IActionResult CallObjectEditor(string _dsobj, int _tabnum, int Objtype)
+        {
+            return ViewComponent("CodeEditor", new {dsobj = _dsobj, tabnum = _tabnum, type = Objtype });
         }
 
     }

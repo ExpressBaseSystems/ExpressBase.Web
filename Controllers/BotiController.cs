9adfacbd8603f6a9cb4ef64418b32afaaf4d1845
Jith Job K.I <jithjob@expressbase.com>
2017-11-17 19:42:02 +0530

    minor

--- Controllers/BotiController.cs
+++ Controllers/BotiController.cs
@@ -22,12 +22,6 @@ namespace ExpressBase.Web.Controllers
             return View();
         }
 
-        [HttpGet]
-        public IActionResult BotList()
-        {
-            return View();
-        }
-
         [HttpPost]
         public IActionResult addBot(string _name, string _url, string _sol_id, string _wel_msg)
         {
@@ -42,6 +36,47 @@ namespace ExpressBase.Web.Controllers
             ViewBag.url = _url;
             ViewBag.welcomemsg = _wel_msg;
             ViewBag.chatid = res.BotId;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult BotList()
+        {
+            int _sol_id = 100;
+            var bot = new BotListRequest();
+            bot.SolutionId = _sol_id;
+            List<ChatBot> Bots = ServiceClient.Get<BotListResponse>(bot).Data;
+
+            string _html = string.Empty;
+
+            string BotTile = @"<div class='botlist-box'>
+            <div class='Bot-tile'>
+                <h4>@Name@</h4>
+                <div class='bottile-icon'></div>
+                <div class='sitename'>@WebsiteURL@</div>
+                <div class='pull-left'>
+                    <div class='created-lbl'>@CreatedBy@</div>
+                    <div class='created-date'>@CreatedAt@</div>
+                </div>
+                <div class='pull-right'>
+
+                    <div class='modified-lbl'>@LastModifiedBy@</div>
+                    <div class='modified-date'>@LastModifiedAt@</div>
+                </div>
+            </div>
+        </div>";
+
+            foreach (ChatBot chatbot in Bots)
+            {
+                _html += BotTile.Replace("@botname@", chatbot.Name)
+                    .Replace("@Name@", chatbot.WebsiteURL)
+                    .Replace("@WebsiteURL@", chatbot.BotId)
+                    .Replace("@LastModifiedBy@", chatbot.LastModifiedBy)
+                    .Replace("@LastModifiedAt@", chatbot.LastModifiedAt.ToString())
+                    .Replace("@CreatedBy@", chatbot.CreatedBy)
+                    .Replace("@CreatedAt@", chatbot.CreatedAt.ToString());
+            }
+            ViewBag.tileshtml = _html;
             return View();
         }
     }

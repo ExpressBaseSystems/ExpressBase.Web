e07a3bf18eea9b3cf37d52abb7ffe90726720b13
Jith Job K.I <jithjob@expressbase.com>
2017-12-14 18:15:56 +0530

    botloader

--- wwwroot/js/ChatBot/ebbot-ext.js
+++ wwwroot/js/ChatBot/ebbot-ext.js
@@ -40,7 +40,12 @@
     iframe.frameBorder = "0";
     iframe.allowFullscreen = true;
     iframecont.appendChild(chatHead);
-    iframecont.appendChild(iframe);
+
+    var loaderDiv = d.createElement("div");
+    loaderDiv.id = "loderdiv";
+
+    loaderDiv.appendChild(iframe);
+    iframecont.appendChild(loaderDiv);
     d.body.appendChild(iframecont);
 
     var chatbtn = d.createElement("div");
@@ -79,11 +84,8 @@
     dpicon.style.height = "40px";
     botdp.appendChild(dpicon);
 
-    var loaderDiv = d.createElement("div");
-
     iframe.onload = function (e) {
-        alert("loaded");
-        loaderDiv.style.display = 'none';
+        iframe.style.visibility = 'visible';
     };
 
     closeDiv.onclick = function () {
@@ -96,8 +98,8 @@
         var ebbot_iframe = document.getElementById("ebbot_iframe");
 
         if (!ebbot_iframe.getAttribute("src")) {
-            //ebbot_iframe.setAttribute("src", "https://expressbase.azurewebsites.net//bote/bot?tid=" + window.EXPRESSbase_SOLUTION_ID);
-            ebbot_iframe.setAttribute("src", "//eb_roby_dev.localhost:5000/bote/bot?tid=" + window.EXPRESSbase_SOLUTION_ID);
+            ebbot_iframe.setAttribute("src", "https://expressbase.azurewebsites.net//bote/bot?tid=" + window.EXPRESSbase_SOLUTION_ID);
+            //ebbot_iframe.setAttribute("src", "//eb_roby_dev.localhost:5000/bote/bot?tid=" + window.EXPRESSbase_SOLUTION_ID);
         }
         if (iframecont.style.display !== "flex") {
             this.style.display = "none";

b9cd16a18e324a85baecc4995880f5406785d1e1
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2017-12-28 20:36:56 +0530

    SecurityController Minor, ManageRoles cshtml,js Modification, TileSetup.js Add

--- Controllers/SecurityController.cs
+++ Controllers/SecurityController.cs
@@ -70,7 +70,7 @@ namespace ExpressBase.Web.Controllers
 			return fr.UserList;
 		}
 
-		public string SaveRole(int _roleId,string _roleName,string _roleDesc,int _appId,string _permission)
+		public string SaveRole(int _roleId,string _roleName,string _roleDesc,int _appId,string _permission, string _role2role, string _users)
 		{
 			Dictionary<string, object> Dict = new Dictionary<string, object>();
 			string return_msg;
@@ -78,9 +78,9 @@ namespace ExpressBase.Web.Controllers
 			Dict["applicationid"] = _appId;
 			Dict["role_name"] = _roleName;
 			Dict["Description"] = _roleDesc;
-			Dict["users"] = string.Empty;
+			Dict["users"] = string.IsNullOrEmpty(_users) ? string.Empty : _users;
 			Dict["permission"] = string.IsNullOrEmpty(_permission) ? string.Empty : _permission;
-			Dict["dependants"] =  string.Empty;
+			Dict["dependants"] = string.IsNullOrEmpty(_role2role) ? string.Empty : _role2role;
 
 			SaveRoleResponse res = this.ServiceClient.Post<SaveRoleResponse>(new SaveRoleRequest { Colvalues = Dict });
 			if (res.id == 0)

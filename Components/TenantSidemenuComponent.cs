0b3f9955a73fd66163fb6e5207b0c71aab27de3f
Jith Job K.I <jithjob@expressbase.com>
2018-01-17 18:28:25 +0530

    TenantSideMenu viewComponent added

---
+++ Components/TenantSidemenuComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Components
+{
+    public class TenantSideMenuViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string refid)
+        {
+            return View();
+        }
+    }
+}

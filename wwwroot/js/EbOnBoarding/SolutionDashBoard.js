7850158280d0beb3591b3cdcbde2b73e4775de2f
Amal K Dev <amaldev@expressbase.com>
2018-01-19 19:21:30 +0530

    solution dashboard

--- wwwroot/js/EbOnBoarding/SolutionDashBoard.js
+++ wwwroot/js/EbOnBoarding/SolutionDashBoard.js
@@ -33,8 +33,9 @@
 
             }
         }).done(function (data) {
-            $("#" + this.whichModal).modal("toggle");
+            $(".dbConnectionEdit tbody").empty();
             this.appenddbConnection(data);
+            $("#" + this.whichModal).modal("toggle");            
         }.bind(this));      
     };
 
@@ -49,9 +50,29 @@
 
             }
         }).done(function (data) {
+            $(".filesDbConnectionEdit tbody").empty();
+            this.appendFilesDb(JSON.parse(data));
             $("#" + this.whichModal).modal("toggle");                                       
         }.bind(this));      
     };
+
+    this.emailConnectionSubmit = function (e) {
+        e.preventDefault();
+        var postData = $(e.target).serializeArray();
+        $.ajax({
+            type: 'POST',
+            url: "../ConnectionManager/SMTP",
+            data: postData,
+            beforeSend: function () {
+
+            }
+        }).done(function (data) {
+            $(".EmailConnectionEdit tbody").empty();
+            $(".EmailConnectionEdit .table-message").remove();
+            this.appendEmailConnection(JSON.parse(data));
+            $("#" + this.whichModal).modal("toggle");
+        }.bind(this));   
+    }
 
     this.appendDataDb = function (object) { 
         var Server = "";
@@ -75,21 +96,37 @@
         else { FilesDB_url = object.FilesDB_url; }
         $(".filesDbConnectionEdit tbody").append(`<tr class="connection-row">
                                                <td field="">Files</td>
-                                        <td field="DatabaseVendor">MongoDB</td>
+                                        <td field="DatabaseVendor">${object.FilesDbVendor}</td>
                                         <td field="FilesDB_url" style="max-width: 337px;text-overflow:ellipsis;overflow: hidden;">${FilesDB_url}</td>
                                         <td field="NickName">${object.NickName}</td>
                                         <td class="edit-row"><button class="btn btn-sm table-btn edit-btn" op="edit" whichmodal="filesDbConnectEdit"><i class="fa fa-pencil"></i></button></td>
                                             </tr>`);
+    };
+    this.appendEmailConnection = function (object) {  
+        if ($.isEmptyObject(object))
+            $(".EmailConnectionEdit").parent().append(`<div class="table-message">No email Accounts added..</div>`);
+        else
+        $(".EmailConnectionEdit tbody").append(`<tr>
+                                        <td field="EmailVendor">${object.ProviderName}</td>
+                                        <td field="Email">${EmailAddress}</td>                                        
+                                        <td field="Password">${object.Password}</td>
+                                        <td field="SMTP">${object.Smtp}</td>
+                                        <td field="Port">${object.Port}</td>
+                                        <td field="NickName">${object.NickName}</td>
+                                        <td class="edit-row"><button whichmodal="EmailConnectionEdit" class="btn btn-sm table-btn edit-btn"><i class="fa fa-pencil"></i></button></td>
+                                    </tr>`);
     };
 
 
     this.init = function () {
         this.appendDataDb(this.Connections.DataDbConnection);
         this.appendFilesDb(this.Connections.FilesDbConnection);
+        this.appendEmailConnection(this.Connections.SMTPConnection);
         $(".s-dash-bodyComm .edit-btn").on("click", this.editConnectionRow.bind(this));
-        //$(".addConnection").on("click", this.addConnectionRow.bind(this));
+        $(".addConnection").on("click", this.addConnectionRow.bind(this));
         $("#dbConnectionSubmit").on("submit", this.dbconnectionsubmit.bind(this));
         $("#filesDbConnectionSubmit").on("submit", this.FilesDbSubmit.bind(this));
+        $("#EmailConnectionSubmit").on("submit", this.emailConnectionSubmit.bind(this));
     };
 
     this.init();

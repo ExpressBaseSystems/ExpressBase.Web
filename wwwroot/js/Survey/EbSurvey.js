f8d0e538d9dcfe941e44f740e6a4f33078f9c4c1
robyabraham <robyabraham@expressbase.com>
2018-02-07 21:01:11 +0530

    survey related...

--- wwwroot/js/Survey/EbSurvey.js
+++ wwwroot/js/Survey/EbSurvey.js
@@ -2,39 +2,60 @@
 
     this.init = function () {
         $(".question-new").off("click").on("click", this.openNewQuestion.bind(this));
+        this.maxChoice = $("#maxchoice").val();
     };
 
     this.openNewQuestion = function () {
         $("#questionModal").modal("show");
-        $(".bootstrap-select a").off("click").on("click", this.changeQuestionType.bind(this));
-        $(".bootstrap-select a:eq(0)").trigger("click");
+        $(".qst-type").off("click").on("click", this.changeQuestionType.bind(this));
+        $("#maxchoice").bind('keyup mouseup', function () {
+            this.maxChoice = $("#maxchoice").val();
+            if ($(".question-choice").length < this.maxChoice) {
+                this.appendNext();
+            }
+            else if ($(".question-choice").length > this.maxChoice)
+                alert("errorrr......manually delete choice");
+        }.bind(this));
+        //$(".bootstrap-select a:eq(0)").trigger("click");
     };
 
-    this.changeQuestionType = function (e) {
-        this.option = $(e.target).text();
-        $("#question-type").empty();
-        if (this.option === "Multiple Choice") {
-            $("#question-type").append(`<div class="question-Choices"></div>`);
-            $(".question-Choices").append(`<div class='choice'><input type="text" placeholder="New choice" class="question-choice"/></div>`);
+    this.changeQuestionType = function () {
+        this.option = $(event.target).text().trim();
+        $(".qst-opt-cont").empty();
+        if (this.option === "Multiple choice(Single-Select)") {
+            //$(".qst-opt-cont").append(`<div class="question-Choices"></div>`);
+            $(".qst-opt-cont").append(`<div class='choice'><input type="text" placeholder="New choice" class="question-choice"/></div>`);
             $(".question-choice").off("keyup").on("keyup", this.appendNext.bind(this));
         }
         else if (this.option === "Yes/No") {
-            $("#question-type").append(`<div class="question-Choices"></div>`);
+            $(".qst-opt-cont").append(`<div class="question-Choices"></div>`);
             $(".question-Choices").append(`<div class='choice'><textarea class="question-choice question-choice-yesno" placeholder="Yes"></textarea></div>`);
             $(".question-Choices").append(`<div class='choice'><textarea class="question-choice question-choice-yesno" placeholder="No"></textarea></div>`);
         }
         else if (this.option === "Text") {
-            $("#question-type").append(`<div class="question-Choices"></div>`);
+            $(".qst-opt-cont").append(`<div class="question-Choices"></div>`);
             $(".question-Choices").append(`<div class='choice' style="width:100%"><textarea class="question-choice question-choice-text" placeholder="Text Input" disabled></textarea></div>`);
         }
         
     };
 
     this.appendNext = function (e) {
-        if ($(e.target).is($(".question-choice").last())) {
-            $(".question-Choices").append(`<div class='choice'><input type="text" placeholder="New choice" class="question-choice"/></div>`);
-            $(".question-choice").off("keyup").on("keyup", this.appendNext.bind(this));
+        if (e === undefined) {
+            this.appendChoice();
         }
+        else {
+            if ($(e.target).is($(".question-choice").last())) {
+                if ($(".question-choice").length < this.maxChoice) {
+                    this.appendChoice();
+                }
+            }
+        }
+    };
+
+    this.appendChoice = function () {
+        $(".qst-opt-cont").append(`<div class='choice'><input type="text" placeholder="New choice" class="question-choice"/></div>`);
+        $(".question-choice").off("keyup").on("keyup", this.appendNext.bind(this));
+        $(".question-choice").last().focus();
     };
 
     this.init();

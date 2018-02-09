95a0099c2b6528521e728a11142bdeed0a1ca089
Amal K Dev <amaldev@expressbase.com>
2018-02-09 19:17:20 +0530

    app  dashboard final

--- wwwroot/js/EbDashBoards/AppDashWeb.js
+++ wwwroot/js/EbDashBoards/AppDashWeb.js
@@ -6,27 +6,36 @@
 
     this.apndObTypeClpsCont = function () {
         for (var otype in this.ObjTypes) {
-            this.objectTab.append(`<div class="connectons-container col-md-6" style="width:50%;">
-                        <div class="s-dash-headComm form-inline app-dash-clps-head" data-toggle="collapse" data-target="#obtype_container${otype}" style="cursor:pointer;">
+            this.objectTab.append(`<div class="connectons-container col-md-6" style="width:33.3%;">
+                        <div class="s-dash-headComm form-inline app-dash-clps-head" obj_container="obtype_container${otype}" data-toggle="collapse" data-target="#obtype_container${otype}" style="cursor:pointer;">
                             <img src="/images/svg/${this.objTypesImages[otype].ImgSrc}" class="obtype_img">
                             ${otype} <div class="ob_count" id="ob_count${otype}"></div>
                             <button class="btn btn-sm table-btn pull-right newob_create"><i class="fa fa-plus"></i></button>
                             <button class="btn btn-sm table-btn pull-right ob_search"><i class="fa fa-search"></i></i></button>   
-                            <input type="text" class="form-control obj_search_input pull-right" placeholder="Search" style="display:none;">
+                            <input type="text" search_body="obtype_container${otype}" class="form-control obj_search_input pull-right" placeholder="Search" style="display:none;">
                         </div>
                         <div class="s-dash-bodyComm appdash-extra-style collapse" id="obtype_container${otype}">
-                            <div class="col-md-4 col-lg-4 col-sm-4 col-xs-12 pd-b-15">
-                                <div class="appdash-obj-container pd-0">
-                                    <div class="col-md-2"><img src="~/images/svg/chat1.svg" /></div>
-                                    <div class="col-md-8">Bot Forms</div>
-                                    <div class="col-md-2"><i class="fa fa-plus"></i></div>
-                                </div>
-                            </div>                            
+                        <div class="not-found" style="display:none;"></div>
                         </div>
-                    </div>`);
-            $("#ob_count" + otype).text("(" + this.getObjCount($("#obtype_container" + otype)) + ")");
+                    </div>`);            
+            this.appendObjectList($("#obtype_container" + otype), this.ObjTypes[otype], otype);
         }
         $(".s-dash-headComm").on("click", this.actionCollapse.bind(this));
+    };
+
+    this.appendObjectList = function ($listContaner, otypeInt,otypeText) {
+        if (!$.isEmptyObject(this.ObjCollection[otypeInt])){
+            for (var i = 0; i < this.ObjCollection[otypeInt].Objects.length; i++) {
+                $listContaner.append(`<div class="col-md-6 col-lg-6 col-sm-6 col-xs-12 pd-b-15">
+                                <div class="appdash-obj-container pd-0">                                   
+                                    <div class="col-md-10"><div class="obj_name">${this.ObjCollection[otypeInt].Objects[i].ObjName}</div>
+                                        <div class="obj-desc">${this.ObjCollection[otypeInt].Objects[i].Description}</div></div>
+                                    <div class="col-md-2 center_align_flex"><i obj-id="${this.ObjCollection[otypeInt].Objects[i].Id}" class="fa fa-edit"></i></div>
+                                </div>
+                            </div>`);
+            }
+            $("#ob_count" + otypeText).text("(" + this.getObjCount($("#obtype_container" + otypeText)) + ")");
+        }    
     };
 
     this.getObjCount = function (objCont) {
@@ -35,15 +44,21 @@
     };
 
     this.actionCollapse = function (e) {
-        if ($(e.target).attr("aria-expanded") !== "true") {
-            $(e.target).parent().animate({ width: '100%' });
-            $(e.target).children(".obj_search_input").show();
-            $(e.target).children(".ob_search").hide();
+        var $div = $(e.target).closest(".s-dash-headComm");
+        if ($div.attr("aria-expanded") !== "true") {
+            $div.parent().animate({ width: '100%' });
+            $div.addClass("change_bg_onchange").children(".obj_search_input").show();
+            $div.children(".ob_search").hide();
+            if (this.getObjCount($("#" + $div.attr("obj_container"))) <= 1)
+                $("#" + $div.attr("obj_container")).children(".not-found").show().text("empty.");
+            else
+                $("#" + $div.attr("obj_container")).children(".not-found").hide().text(" ");
         }
         else {
-            $(e.target).parent().animate({ width: '50%' });
-            $(e.target).children(".obj_search_input").hide();
-            $(e.target).children(".ob_search").show();
+            $div.parent().animate({ width: '33.3%' });
+            $div.removeClass("change_bg_onchange");
+            $div.children(".obj_search_input").hide();
+            $div.children(".ob_search").show();
         }
     };
 
@@ -63,11 +78,31 @@
         }
     }
 
+    this.searchObjects = function (e) {
+        var srchBody = $(e.target).attr("search_body");
+        var srch = $(e.target).val().toLowerCase();
+        var count = 0;
+        $.each($("#"+srchBody).children(), function (i, obj) {
+            var cmpstr = $(obj).children().find(".obj_name").text().toLowerCase();
+            if (cmpstr.indexOf(srch) !== -1) {
+                $(obj).show();
+                count++;
+            }
+            else
+                $(obj).hide();
+        });
+        if (count === 0)
+            $("#" + srchBody + " .not-found").show().text("Item not found....");
+        else
+            $("#" + srchBody + " .not-found").hide().text("");
+    };
+
     this.start_exe = function () {
         this.apndObTypeClpsCont();
         $(".newob_create").on("click", this.createNewObject.bind(this));
         $(".ob_search").on("click", this.searchObjectClick.bind(this));
         $(".obj_search_input").on("click", function (e) { e.stopPropagation(); });
+        $(".obj_search_input").off("keyup").on("keyup", this.searchObjects.bind(this));
     }
 
     this.start_exe();

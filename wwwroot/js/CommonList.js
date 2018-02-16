9e4ae2cd6d2f30bdc63325ccb35d3ca6a1764e68
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-02-16 18:36:47 +0530

    Anonymous USer Management

--- wwwroot/js/CommonList.js
+++ wwwroot/js/CommonList.js
@@ -12,10 +12,19 @@
         var tbldata = [];
         tblcols.push({ data: null, title: "Serial No", className: "dataTableColumnStyle", width: '30px', searchable: false, orderable: false });
         if (this.metadata.indexOf("_profPic") !== -1)
-            tblcols.push({ data: null, title: "", className: "dataTableColumnStyle text-center", width: '60px',render: this.tblProfPicRender, searchable: false, orderable: false });
+            tblcols.push({ data: null, title: "", className: "dataTableColumnStyle text-center", width: '60px', render: this.tblProfPicRender, searchable: false, orderable: false });
+        if (this.metadata.indexOf("_fbProfPic") !== -1) {
+            tblcols.push({ data: null, title: "", className: "dataTableColumnStyle text-center", width: '60px', render: this.tblFbProfPicRender, searchable: false, orderable: false });
+            tblcols.push({ data: parseInt(this.metadata[0]) + 1, title: this.metadata[parseInt(this.metadata[0]) + 1], visible: false });
+        }
+            
+
         tblcols.push({ data: 1, title: this.metadata[1], visible: false });
-        for (var i = 2; i <= parseInt(this.metadata[0]); i++)
-            tblcols.push({ data: i, title: this.metadata[i].replace("_"," "), className: "dataTableColumnStyle", width: '220px' });
+
+        tblcols.push({ data: 2, title: this.metadata[2].replace("_", " "), className: "dataTableColumnStyle", width: '220px', render: this.tblNameColumnRender });
+
+        for (var i = 3; i <= parseInt(this.metadata[0]); i++)
+            tblcols.push({ data: i, title: this.metadata[i].replace("_"," "), className: "dataTableColumnStyle", width: '150px' });
         tblcols.push({ data: null, title: "Edit/View", className: "dataTableColumnStyle", width: '80px', className: "text-center", render: this.tblEditColumnRender, searchable: false, orderable: false });
 
         if (this.metadata.indexOf("_user") !== -1)// to fill tbldata with appropriate data
@@ -27,6 +36,10 @@
         else if (this.metadata.indexOf("_roles") !== -1)
             for (i = 0; i < this.itemList.length; i++)
                 tbldata.push({ 1: this.itemList[i][this.metadata[1]], 2: this.itemList[i][this.metadata[2]], 3: this.itemList[i][this.metadata[3]], 4: this.itemList[i][this.metadata[4]], 5: this.itemList[i][this.metadata[5]], 6: this.itemList[i][this.metadata[6]], 7: this.itemList[i][this.metadata[7]] });
+
+        else if (this.metadata.indexOf("_anonymousUser") !== -1)
+            for (i = 0; i < this.itemList.length; i++)
+                tbldata.push({ 1: this.itemList[i][this.metadata[1]], 2: this.itemList[i][this.metadata[2]], 3: this.itemList[i][this.metadata[3]], 4: this.itemList[i][this.metadata[4]], 5: this.itemList[i][this.metadata[5]], 6: this.itemList[i][this.metadata[6]], 7: this.itemList[i][this.metadata[7]], 8: this.itemList[i][this.metadata[8]], 9: this.itemList[i][this.metadata[9]] });
         
         var tbl = "#tblCommonList";
         var table = $(tbl).DataTable({
@@ -45,7 +58,7 @@
                 cell.innerHTML = i + 1;
             });
         }).draw();
-        $("#tblCommonList").on('click', '.fa-pencil', this.onClickEdit.bind(this));
+        $("#tblCommonList").on('click', '.editviewclass', this.onClickEdit.bind(this));
     }
 
     this.onClickEdit = function (e) {
@@ -59,14 +72,27 @@
         else if (this.metadata.indexOf("_userGroup") !== -1) {
             window.open("../Security/ManageUserGroups?itemid=" + id, "_blank");
         } 
+        else if (this.metadata.indexOf("_anonymousUser") !== -1) {
+            window.open("../Security/ManageAnonymousUser?itemid=" + id, "_blank");
+        } 
 
+    }
+
+    this.tblNameColumnRender = function (data, type, row, meta) {
+        return `<div class="editviewclass" style="cursor:pointer;" data-id=${row[1]}>${data}</div>`;
     }
 
     this.tblEditColumnRender = function (data, type, row, meta) {
-        return `<i class="fa fa-pencil fa-2x" aria-hidden="true" style="cursor:pointer;" data-id=${data[1]}></i>`;
+        return `<i class="fa fa-pencil fa-2x editviewclass" aria-hidden="true" style="cursor:pointer;" data-id=${data[1]}></i>`;
     }
     this.tblProfPicRender = function (data, type, row, meta) {
         return `<img class='img-thumbnail pull-right' src='../static/dp/dp_${data[1]}_micro.jpg' />`;
+    }
+    this.tblFbProfPicRender = function (data, type, row, meta) {
+        var id = data[9];
+        if (id == "")
+            id = '12345678';
+        return `<img class='img-thumbnail pull-right' src='http://graph.facebook.com/${id}/picture?type=square' />`;
     }
 
 

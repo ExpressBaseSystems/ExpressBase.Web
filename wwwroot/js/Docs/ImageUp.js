8785df392171b40b9bdf23bab31ab194d8be152d
Amal K Dev <amaldev@expressbase.com>
2018-03-23 20:10:53 +0530

    download link

--- wwwroot/js/Docs/ImageUp.js
+++ wwwroot/js/Docs/ImageUp.js
@@ -58,15 +58,27 @@
     this.drawThumbNails = function (collection) {
         for (var i = 0; i < collection.length; i++) {
             var im = collection[i];
-            $("#image_down_cont_body").append(` <div class="img_wrapper">
-                    <div class="img_wrapper_img">
-                        <img src="http://${ this.TenantId }-dev.localhost:41500/static/${ im.objectId }.jpg" class="img-responsive" />
+            $("#image_down_cont_body").append(`
+                <div class="img_wrapper">
+                <div class="img_wrapper_img">
+                    <img src="http://${ this.TenantId}-dev.localhost:41500/static/${im.objectId }.jpg" class="img-responsive" />
+                </div>
+                <div class="img_wrapper_text">
+                    <h5 class="f text-center">amal.jpg (<span class="kb">${im.length / 1000}Kb</span>)</h5>
+                    <p class="f_s text-center" data-toggle="tooltip" title="${im.uploadDateTime}"><i class="fa fa-upload"></i> ${im.uploadDateTime}</p>
+                </div>
+                <div class="btn_container form-inline">
+                    <div class="dropdown" style="float:left;margin-right:5px">
+                        <button type='button' data-toggle="dropdown" class='kv-file-upload btn btn-kv btn-default btn-outline-secondary dropdown-toggle' title='Download'><i class="fa fa-download"></i></button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Orgninal</a></li>
+                            <li><a href="#">Medium</a></li>
+                            <li><a href="#">small</a></li>
+                        </ul>
                     </div>
-                    <div class="img_wrapper_text">
-                        <h5 class="f">amal.jpg(${im.length/1000}Kb)</h5>
-                        <p class="f_s text-center">Uploaded on ${im.uploadDateTime}</p>
-                    </div>
-                </div>`);
+                    <button type='button' class='kv-file-upload btn btn-kv btn-default btn-outline-secondary' title='Tag'>Tag</button>
+                </div>
+                    </div>`);
         }
     };
 

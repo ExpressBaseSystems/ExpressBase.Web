11db263a772323b47f0bd9c6d06971c24cab899e
Ahammed Unni <ahammedunni@expressbase.com>
2018-04-21 00:28:59 +0530

    Token Sec

--- BaseControllers/EbBaseController.cs
+++ BaseControllers/EbBaseController.cs
@@ -8,6 +8,8 @@ using ServiceStack;
 using ServiceStack.Redis;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ExpressBase.Web.BaseControllers
 {
@@ -86,7 +88,7 @@ namespace ExpressBase.Web.BaseControllers
             base.OnActionExecuting(context);
         }
 
-        public bool IsTokenValid(string rtoken)
+        public bool IsTokenNotExpired(string rtoken)
         {
             bool isvalid = false;
             var jwtToken = new JwtSecurityToken(rtoken);
@@ -99,6 +101,51 @@ namespace ExpressBase.Web.BaseControllers
                 isvalid = true;
             }
             return isvalid;
+        }
+
+        public bool VerifySignature(string token)
+        {
+            string PublicKey = Environment.GetEnvironmentVariable(EnvironmentConstants.EB_JWT_PUBLIC_KEY_XML);
+            int pos1 = PublicKey.IndexOf("<Modulus>");
+            int pos2 = PublicKey.IndexOf("</Modulus>");
+            int pos3 = PublicKey.IndexOf("<Exponent>");
+            int pos4 = PublicKey.IndexOf("</Exponent>");
+            string modkeypub = PublicKey.Substring(pos1 + 9, pos2 - pos1 - 9);
+            string expkeypub = PublicKey.Substring(pos3 + 10, pos4 - pos3 - 10);
+
+            try
+            {
+                string[] tokenParts = token.Split('.');
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(
+                  new RSAParameters()
+                  {
+                      Modulus = FromBase64Url(modkeypub),
+                      Exponent = FromBase64Url(expkeypub)
+                  });
+
+                SHA256 sha256 = SHA256.Create();
+                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(tokenParts[0] + '.' + tokenParts[1]));
+
+                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm("SHA256");
+                if (rsaDeformatter.VerifySignature(hash, FromBase64Url(tokenParts[2])))
+                {
+                    Console.WriteLine("Signature is verified");
+                    return true;
+                }
+            }
+            catch (Exception e) { Console.WriteLine("Exception from VerifySignature:" + e.ToString()); }
+            return false;
+        }
+
+        public byte[] FromBase64Url(string base64Url)
+        {
+            string padded = base64Url.Length % 4 == 0
+                ? base64Url : base64Url + "====".Substring(base64Url.Length % 4);
+            string base64 = padded.Replace("_", "/")
+                                  .Replace("-", "+");
+            return Convert.FromBase64String(base64);
         }
     }
 }

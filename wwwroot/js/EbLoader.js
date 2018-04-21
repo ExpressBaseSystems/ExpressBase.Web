3024747b0088354dd69618af727300194ace6ddd
Amal K Dev <amaldev@expressbase.com>
2018-04-21 18:35:26 +0530

    loader ebloder.js

---
+++ wwwroot/js/EbLoader.js
@@ -0,0 +1,38 @@
+﻿(function ($) {
+    /*register loader*/
+    $.fn.EbLoader = function (action,options) {
+        /*the target*/
+        var el = $(this);
+
+        var operation = action;
+
+        var settings = $.extend({
+            color:"#ec9351",
+            bgColor: 'transparent', // Default background color
+            fontAwesome: null, // Default null
+            imagePath: null // Default Path custom image
+        }, options);
+
+        //Apply styles
+        el.css("background-color", settings.bgColor);
+
+        if (!el.hasClass('eb-loader-prcbar'))
+            el.addClass('eb-loader-prcbar');
+       
+        //show function for processbar
+        function showPrc() {
+            el.show();
+        };
+        
+        //hide function for processbar
+        function hidePrc() {
+            el.hide();
+        };
+
+        if (operation = "show")
+            showPrc();
+        else if (operation = "hide")
+            hidePrc();
+        else return null;    
+    };
+}(jQuery));

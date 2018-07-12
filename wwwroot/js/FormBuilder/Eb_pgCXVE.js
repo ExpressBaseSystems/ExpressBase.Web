aaa31746acbdb9a1f1a14d22a5f572421d531ce3
Jith Job K.I <jithjob@expressbase.com>
2018-07-12 12:23:38 +0530

    minor

--- wwwroot/js/FormBuilder/Eb_pgCXVE.js
+++ wwwroot/js/FormBuilder/Eb_pgCXVE.js
@@ -44,8 +44,11 @@
         this.reDrawRelatedPGrows();
         this.PGobj.OnInputchangedFn.bind(this.PGobj)();
         if ((this.editor > 6 && this.editor < 15) || (this.editor > 15 && this.editor < 15)) {
-            let func = this.PGobj.OnChangeExec[_CurProp].bind(PropsObj, this.PGobj);
-            func();// call Onchange exec for non inp field CXVEs
+            let func = this.PGobj.OnChangeExec[_CurProp]
+            if (func) {
+                func.bind(PropsObj, this.PGobj);
+                func();// call Onchange exec for non inp field CXVEs
+            }
         }
     };
 

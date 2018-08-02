2a967aee962909400fed9fa52b62bc68bda7ab42
george-expressbase <george@expressbase.com>
2018-08-02 16:23:54 +0530

    comment  removal

--- Controllers/PayPalController.cs
+++ Controllers/PayPalController.cs
@@ -21,74 +21,8 @@ using ExpressBase.Common.Enums;
 
 namespace ExpressBase.Web.Controllers
 {
-    //public enum HttpMethods { GET, POST, PUT, DELETE, HEAD, CONNECT, OPTIONS, PATCH };
-    //public enum PlanType { FIXED, INFINITE };
-    //public enum PlanState { CREATED, ACTIVE, INACTIVE };
-    //public enum TermType { MONTHLY, WEEKLY, YEARLY };
-    //public enum PaymentMethod { bank, paypal };
-    //public enum CardState { expired, ok };
-
     public class PayPalController : EbBaseIntCommonController
     {
-        //private const string OAuthTokenPath = "/v1/oauth2/token";
-        //public static string Response;
-        //public static int StatusCode;
-        //public static string P_PayResponse;
-        //public static int P_PayResCode;
-
-        //public static System.IO.Stream GenerateStreamFromString(string s)
-        //{
-        //    var stream = new System.IO.MemoryStream();
-        //    var writer = new System.IO.StreamWriter(stream);
-        //    writer.Write(s);
-        //    writer.Flush();
-        //    stream.Position = 0;
-        //    return stream;
-        //}
-
-        //public static System.IO.Stream GenerateStreamFromObject(object s)
-        //{
-        //    var stream = new System.IO.MemoryStream();
-        //    var writer = new System.IO.StreamWriter(stream);
-        //    writer.Write(s);
-        //    writer.Flush();
-        //    stream.Position = 0;
-        //    return stream;
-        //}
-
-        //public static string GetStringFromStream(System.IO.Stream stream)
-        //{
-        //    System.IO.StreamReader reader = new System.IO.StreamReader(stream);
-        //    string str = reader.ReadToEnd();
-        //    return str;
-        //}
-
-        //private static async Task<string> GetResponseContents(HttpResponseMessage Response)
-        //{
-        //    return await Response.Content.ReadAsStringAsync();
-        //}
-
-        //private static void PayPalCallback(IRestResponse response, RestRequestAsyncHandle handle)
-        //{
-        //    if (response.StatusCode == HttpStatusCode.OK)
-        //    {
-        //        Response = response.Content;
-        //        StatusCode = (int)response.StatusCode;
-        //    }
-        //    else
-        //    {
-        //        StatusCode = (int)response.StatusCode;
-        //        Console.WriteLine("\nRequest Failed");
-        //    }
-        //}
-
-        //private static async Task<HttpResponseMessage> Send(HttpMethod _method, Flurl.Http.FlurlRequest flurlRequest, string JsonBody)
-        //{
-        //    return await flurlRequest.SendAsync(_method, new Flurl.Http.Content.CapturedJsonContent(JsonBody));
-        //}
-
-
-
         public PayPalController(IServiceClient _ssclient, IRedisClient _redis) : base(_ssclient, _redis)
         {
         }

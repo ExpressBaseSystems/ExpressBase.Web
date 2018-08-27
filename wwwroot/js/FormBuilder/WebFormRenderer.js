cd2ce76f38ab41ea9e30388a30f39cbc83feb923
Jith Job K.I <jithjob@expressbase.com>
2018-08-27 17:41:49 +0530

    webForm controls initialization, UIchange , serialization small case reoved, EbOnChangeUIfns and Allmetas included

--- wwwroot/js/FormBuilder/WebFormRenderer.js
+++ wwwroot/js/FormBuilder/WebFormRenderer.js
@@ -2,54 +2,62 @@
     this.FormObj = option.formObj;
     this.$saveBtn = option.$saveBtn;
     this.flatControls = option.flatControls;
+    this.initControls = new InitControls(this);
+
+    this.updateCtrlUI = function (cObj) {
+        $.each(cObj, function (prop, val) {
+            //prop = prop.charAt(0).toUpperCase() + prop.slice(1);
+            let meta = getObjByval(AllMetas["Eb" + cObj.ObjType], "name", prop);
+            if (meta) {
+                let NSS = meta.UIChangefn;
+                if (NSS) {
+                    let NS1 = NSS.split(".")[0];
+                    let NS2 = NSS.split(".")[1];
+                    EbOnChangeUIfns[NS1][NS2]("cont_" + cObj.Name, cObj);
+                }
+            }
+        });
+    };
+
+    this.initFormCtrl = function (control) {
+            if (this.initControls[control.ObjType] !== undefined)
+                this.initControls[control.ObjType](control);
+            //$("#" + control.Name).on("blur", this.makeReqFm.bind(this, control)).on("focus", this.removeReqFm.bind(this, control));
+    }.bind(this);
 
     this.init = function () {
         this.$saveBtn.on("click", this.saveForm.bind(this));
         //this.flatControls = this.getFlatControls();
 
         $.each(this.flatControls, function (k, cObj) {
-            if (cObj.objType === 'Date') {
-                var $input = $("[name=" + cObj.name + "]");
-                if (cObj.showDateAs_ === 1) {
-                    $input.MonthPicker({ Button: $input.next().removeAttr("onclick") });
-                    $input.MonthPicker('option', 'ShowOn', 'both');
-                    $input.MonthPicker('option', 'UseInputMask', true);
-                }
-                else {
-                    //$input.mask("0000-00-00");
-                    $input.datetimepicker({ timepicker: false, format: "Y-m-d" });
-                    $input.next().children('i').off('click').on('click', function () { $input.datetimepicker('show'); });
-                }
-            }
-            //else if (cObj.objType === 'Numeric') {
+            this.updateCtrlUI(cObj);
+            this.initFormCtrl(cObj);
+            //if (cObj.ObjType === 'Date') {
             //    var $input = $("[name=" + cObj.name + "]");
+            //    if (cObj.showDateAs_ === 1) {
+            //        $input.MonthPicker({ Button: $input.next().removeAttr("onclick") });
+            //        $input.MonthPicker('option', 'ShowOn', 'both');
+            //        $input.MonthPicker('option', 'UseInputMask', true);
+            //    }
+            //    else {
+            //        //$input.mask("0000-00-00");
+            //        $input.datetimepicker({ timepicker: false, format: "Y-m-d" });
+            //        $input.next().children('i').off('click').on('click', function () { $input.datetimepicker('show'); });
+            //    }
             //}
-            else if (cObj.objType === 'ComboBox') {
-                var $input = $("[name=" + cObj.name + "]");
+            ////else if (cObj.ObjType === 'Numeric') {
+            ////    var $input = $("[name=" + cObj.name + "]");
+            ////}
+            //else if (cObj.ObjType === 'ComboBox') {
+            //    var $input = $("[name=" + cObj.name + "]");
 
-                Vue.component('v-select', VueSelect.VueSelect);
-                Vue.config.devtools = true;
+            //    Vue.component('v-select', VueSelect.VueSelect);
+            //    Vue.config.devtools = true;
 
-                $(`#${cObj.name}_loading-image`).hide();
-                //MakeCaps(cObj);
-                var EbCombo = new EbSelect(cObj);
-            }  
-        }.bind(this));
-    };
-
-    this.getFlatControls = function () {
-        let coll = [];
-        this.ProcRecur(this.FormObj, coll);
-        return coll;
-    };
-
-    this.ProcRecur = function (src_obj, dest_coll) {
-        $.each(src_obj.controls.$values, function (i, obj) {
-            if (obj.isContainer) {
-                this.ProcRecur(obj, dest_coll);
-            }
-            else
-                dest_coll.push(obj);
+            //    $(`#${cObj.name}_loading-image`).hide();
+            //    //MakeCaps(cObj);
+            //    var EbCombo = new EbSelect(cObj);
+            //}
         }.bind(this));
     };
 
@@ -62,7 +70,7 @@
             var _type = obj.ebDbType;
             var _val = (_type === 7) ? parseInt($("#" + obj.name).val()) : $("#" + obj.name).val();
             var _autoic = obj.autoIncrement;
-            FVWTcoll.push({ Name: _name, Value: _val, Type: _type, AutoIncrement: _autoic});
+            FVWTcoll.push({ Name: _name, Value: _val, Type: _type, AutoIncrement: _autoic });
         }.bind(this));
         return FVWTcoll;
     };

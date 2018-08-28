de602a6d71ada26609943301759ef9eb25827260
george-expressbase <george@expressbase.com>
2018-08-28 20:03:52 +0530

    File Download Controller added to trigger download

---
+++ Controllers/FileDownloadController.cs
@@ -0,0 +1,26 @@
+﻿using ExpressBase.Common.ServiceStack.ReqNRes;
+using ExpressBase.Web.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Controllers
+{
+    public class FileDownloadController : EbBaseIntCommonController
+    {
+        public FileDownloadController(IServiceClient _ssclient, IRedisClient _redis) : base(_ssclient, _redis)
+        {
+        }
+        
+        [HttpGet]
+        public void Download()
+        {
+            FileDownloadRequestObject req = new FileDownloadRequestObject();
+            var DownloadRes = this.ServiceClient.Post<FileDownloadResponseObject>(req);
+        }
+    }
+}

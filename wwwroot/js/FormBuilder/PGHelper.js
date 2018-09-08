3876487699aacf26a6c476f027ba45bc4cd1acf4
Jith Job K.I <jithjob@expressbase.com>
2018-09-08 13:13:18 +0530

    pghelper.dataSourceInit() completed

--- wwwroot/js/FormBuilder/PGHelper.js
+++ wwwroot/js/FormBuilder/PGHelper.js
@@ -8,24 +8,27 @@
             url: "../DS/GetColumns",
             data: { DataSourceRefId: this.PGobj.PropsObj.DataSourceId },
             success: function (Columns) {
-                this.PGobj.PropsObj["Columns"] = JSON.parse(Columns);
                 this.clearDependantProps("Columns");
+                this.PGobj.PropsObj["Columns"] = JSON.parse(Columns);
                 this.PGobj.refresh();
                 fun(this.PGobj.PropsObj);
                 $.LoadingOverlay('hide');
-            }
+            }.bind(this)
         });
-    };
+    }.bind(this);
 
     this.clearDependantProps = function (prop) {
         let names = [];
-        AllMetas[this.PGobj.CurProp].forEach(function (meta, i) {
+        this.PGobj.Metas.forEach(function (meta, i) {
             if (meta.source === prop)
-                this.clearProp(meta.name);
-        });
+                this.clearProp(meta);
+        }.bind(this));
     }
 
-    this.clearProp = function () {
-
+    this.clearProp = function (meta) {
+        if (meta.editor === 24 || meta.editor === 8 && meta.Limit !== 1)
+            this.PGobj.PropsObj[meta.name].$values = [];
+        else if (meta.editor === 8 && meta.Limit === 1)
+            this.PGobj.PropsObj[meta.name] = null;
     };
 };

62e57bfe632553bab58a3fe2180303eaa17205ff
Roby Abraham <robyabraham@expressbase.com>
2018-10-04 15:22:17 +0530

    logging

--- Components/DataVisualizationViewComponent.cs
+++ Components/DataVisualizationViewComponent.cs
@@ -61,46 +61,59 @@ namespace ExpressBase.Web.Components
         private EbDataVisualization getDVObject(EbDataVisualization dvobj)
         {
             //DataSourceColumnsResponse columnresp = null;
-            DataSourceColumnsResponse columnresp = this.Redis.Get<DataSourceColumnsResponse>(string.Format("{0}_columns", dvobj.DataSourceRefId));
-            Console.WriteLine("Columns;;;" + columnresp);
-            Console.WriteLine("Columns;;;" + dvobj.EbDataSource);
-            if (columnresp == null || columnresp.Columns.Count == 0)
-                columnresp = this.ServiceClient.Get<DataSourceColumnsResponse>(new TableColumnsRequest { RefId = dvobj.DataSourceRefId, SolnId = ViewBag.cid, Params = (dvobj.EbDataSource.FilterDialog != null) ? dvobj.EbDataSource.FilterDialog.GetDefaultParams() : null });
-
-
-            var __columns = (columnresp.Columns.Count > 1) ? columnresp.Columns[1] : columnresp.Columns[0];
-            int _pos = __columns.Count + 100;
-
-            var Columns = new DVColumnCollection();
-            dvobj.IsPaged = columnresp.IsPaged.ToString();
-
-            var indx = -1;
-            foreach (EbDataColumn column in __columns)
+            try
             {
-                DVBaseColumn _col = null;
+                DataSourceColumnsResponse columnresp = this.Redis.Get<DataSourceColumnsResponse>(string.Format("{0}_columns", dvobj.DataSourceRefId));
+                if (columnresp == null || columnresp.Columns.Count == 0)
+                {
+                    Console.WriteLine("Column Object in Redis is null or count 0");
+                    columnresp = this.ServiceClient.Get<DataSourceColumnsResponse>(new TableColumnsRequest { RefId = dvobj.DataSourceRefId, SolnId = ViewBag.cid, Params = (dvobj.EbDataSource.FilterDialog != null) ? dvobj.EbDataSource.FilterDialog.GetDefaultParams() : null });
+                    if (columnresp == null || columnresp.Columns.Count == 0)
+                    {
+                        Console.WriteLine("Column Object from SS is null or count 0");
+                        throw new Exception("Object Not found(Redis + SS)");
+                    }
+                }
 
-                if (column.Type == EbDbTypes.String && column.ColumnName == "socialid")
-                    _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight", RenderAs = StringRenderType.Image };
-                else if (column.Type == EbDbTypes.String && column.ColumnName == "latlong")
-                    _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight", RenderAs = StringRenderType.Marker };
-                else if (column.Type == EbDbTypes.String)
-                    _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
-                else if (column.Type == EbDbTypes.Int16 || column.Type == EbDbTypes.Int32 || column.Type == EbDbTypes.Int64 || column.Type == EbDbTypes.Double || column.Type == EbDbTypes.Decimal || column.Type == EbDbTypes.VarNumeric)
-                    _col = new DVNumericColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight dt-body-right" };
-                else if (column.Type == EbDbTypes.Boolean)
-                    _col = new DVBooleanColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
-                else if (column.Type == EbDbTypes.DateTime || column.Type == EbDbTypes.Date || column.Type == EbDbTypes.Time)
-                    _col = new DVDateTimeColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, sType = "date-uk", Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
-                _col.EbSid = column.Type.ToString() + column.ColumnIndex;
-                Columns.Add(_col);
-                indx = column.ColumnIndex;
+
+                var __columns = (columnresp.Columns.Count > 1) ? columnresp.Columns[1] : columnresp.Columns[0];
+                int _pos = __columns.Count + 100;
+
+                var Columns = new DVColumnCollection();
+                dvobj.IsPaged = columnresp.IsPaged.ToString();
+
+                var indx = -1;
+                foreach (EbDataColumn column in __columns)
+                {
+                    DVBaseColumn _col = null;
+
+                    if (column.Type == EbDbTypes.String && column.ColumnName == "socialid")
+                        _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight", RenderAs = StringRenderType.Image };
+                    else if (column.Type == EbDbTypes.String && column.ColumnName == "latlong")
+                        _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight", RenderAs = StringRenderType.Marker };
+                    else if (column.Type == EbDbTypes.String)
+                        _col = new DVStringColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
+                    else if (column.Type == EbDbTypes.Int16 || column.Type == EbDbTypes.Int32 || column.Type == EbDbTypes.Int64 || column.Type == EbDbTypes.Double || column.Type == EbDbTypes.Decimal || column.Type == EbDbTypes.VarNumeric)
+                        _col = new DVNumericColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight dt-body-right" };
+                    else if (column.Type == EbDbTypes.Boolean)
+                        _col = new DVBooleanColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
+                    else if (column.Type == EbDbTypes.DateTime || column.Type == EbDbTypes.Date || column.Type == EbDbTypes.Time)
+                        _col = new DVDateTimeColumn { Data = column.ColumnIndex, Name = column.ColumnName, sTitle = column.ColumnName, sType = "date-uk", Type = column.Type, bVisible = true, sWidth = "100px", Pos = _pos, ClassName = "tdheight" };
+                    _col.EbSid = column.Type.ToString() + column.ColumnIndex;
+                    Columns.Add(_col);
+                    indx = column.ColumnIndex;
+                }
+                //dvobj.Columns.Add(new DVNumericColumn { Data = ++indx, Name = "RATE_GRAFT", sTitle = "RATE+GRAFT", Type = EbDbTypes.Int32, bVisible = true, sWidth = "100px", ClassName = "tdheight dt-body-right",Formula = "T0.RATE+T0.GRAFT" });
+                if (dvobj.Columns == null || dvobj.Columns.Count == 0)
+                    dvobj.Columns = Columns;
+                else
+                    dvobj.Columns = compareDVColumns(dvobj.Columns, Columns);
+                dvobj.DSColumns = dvobj.Columns;
             }
-            //dvobj.Columns.Add(new DVNumericColumn { Data = ++indx, Name = "RATE_GRAFT", sTitle = "RATE+GRAFT", Type = EbDbTypes.Int32, bVisible = true, sWidth = "100px", ClassName = "tdheight dt-body-right",Formula = "T0.RATE+T0.GRAFT" });
-            if (dvobj.Columns == null || dvobj.Columns.Count == 0)
-                dvobj.Columns = Columns;
-            else
-                dvobj.Columns = compareDVColumns(dvobj.Columns, Columns);
-            dvobj.DSColumns = dvobj.Columns;
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception (GetdvObject): " + e.StackTrace);
+            }
             return dvobj;
         }
 

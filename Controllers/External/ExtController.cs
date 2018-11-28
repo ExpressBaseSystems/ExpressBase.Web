aed76c07a02ce04b761cc30eafa2eacf1b261d46
Ahammed Unni <ahammedunni@expressbase.com>
2018-11-29 00:22:41 +0530

    eb-test.xyz update

--- Controllers/External/ExtController.cs
+++ Controllers/External/ExtController.cs
@@ -416,7 +416,7 @@ namespace ExpressBase.Web.Controllers
             if (bOK2AttemptLogin)
             {
                 string token = req["g-recaptcha-response"];
-                Recaptcha data = await RecaptchaResponse("6LcQuxgUAAAAAD5dzks7FEI01sU61-vjtI6LMdU4", token);
+                Recaptcha data = await RecaptchaResponse(Environment.GetEnvironmentVariable(EnvironmentConstants.EB_RECAPTCHA_SECRET), token);
                 if (!data.Success)
                 {
                     if (data.ErrorCodes.Count <= 0)

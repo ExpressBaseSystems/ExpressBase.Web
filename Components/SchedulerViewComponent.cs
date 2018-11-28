4268cb89217b2d3b8e138285cdfb8ee19f974e73
DonajoseUllattil <donajose@expressbase.com>
2018-11-28 11:36:50 +0530

    Scheduler in Reportbuilder

--- Components/SchedulerViewComponent.cs
+++ Components/SchedulerViewComponent.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using ServiceStack.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +11,20 @@ namespace ExpressBase.Web.Components
 {
     public class SchedulerViewComponent : ViewComponent
     {
+        protected JsonServiceClient ServiceClient { get; set; }
+
+        protected RedisClient Redis { get; set; }
+
+        public SchedulerViewComponent(IServiceClient _client, IRedisClient _redis)
+        {
+            this.ServiceClient = _client as JsonServiceClient;
+            this.Redis = _redis as RedisClient;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
-        {           
+        {
+            GetAllUsersResponse  Res= ServiceClient.Get<GetAllUsersResponse>( new GetAllUsersRequest());
+            ViewBag.users = Res.Users;
+            ViewBag.usergroups = Res.UserGroups;
             return View();
         }
     }

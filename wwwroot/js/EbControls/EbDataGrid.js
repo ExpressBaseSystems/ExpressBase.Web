b54a24496b0b2d62013428c644501f6dbe0b4a7b
Jith Job <jithjob123@hotmail.com>
2019-03-27 23:38:16 +0530

    DG + replaced with check mark

--- wwwroot/js/EbControls/EbDataGrid.js
+++ wwwroot/js/EbControls/EbDataGrid.js
@@ -151,7 +151,7 @@
         }.bind(this));
         tr += `<td class='ctrlstd' mode='${this.mode_s}'>
                     @editBtn@
-                    <span class='check-row rowc' tabindex='1'><span class='fa fa-plus'></span></span>
+                    <span class='check-row rowc' tabindex='1'><span class='fa fa-check'></span></span>
                     <span class='del-row rowc @del-c@' tabindex='1'><span class='fa fa-minus'></span></span>
                 </td></tr>`
             .replace("@editBtn@", anyColEditable ? "<span class='edit-row rowc' tabindex='1'><span class='fa fa-pencil'></span></span>" : "")
@@ -335,7 +335,7 @@
         let rowid = $tr.attr("rowid");
         if (!this.AllRequired_valid_Check(rowid))
             return;
-        $td.find(".check-row").hide().find(".fa-plus").removeClass("fa-plus").addClass("fa-check");
+        $td.find(".check-row").hide();
         $td.find(".del-row").show();
         $td.find(".edit-row").show();
 

49bf8cb03f2a3ae5c4c37e049f6ee26d6b63f0de
Jith Job <jithjob123@hotmail.com>
2019-06-18 19:34:57 +0530

    updateAggCols afterv after addrow

--- wwwroot/js/EbControls/EbDataGrid.js
+++ wwwroot/js/EbControls/EbDataGrid.js
@@ -70,7 +70,7 @@
     };
 
     this.SwitchToViewMode = function () {
-        $(`#${this.TableId} tbody [is-editing=true]`).remove();        
+        $(`#${this.TableId} tbody [is-editing=true]`).remove();
         $(`#${this.TableId} tbody>tr>.ctrlstd`).attr("mode", "view");
         this.mode_s = "view";
     };
@@ -346,6 +346,7 @@
         $tr.show(300);
         this.bindReq_Vali_UniqRow($tr);
         this.setCurRow(rowid);
+        this.updateAggCols(rowid);
         return [$tr, this.initRowCtrls(rowid)];
 
     }.bind(this);
@@ -568,11 +569,11 @@
         let sum = 0;
         $.each($(`#${this.TableId} > tbody [colname='${colname}'] [ui-inp]`), function (i, Iter_Inp) {
             let val;
-            let typing_inp = event.target;
-            //let Iter_Inp = $(span).closest("td").find("[ui-inp]")[0];
 
-            if (typing_inp === Iter_Inp)
+            if (event && event.target === Iter_Inp) {
+                let typing_inp = event.target;
                 val = parseFloat(typing_inp.value);
+            }
             else
                 val = parseFloat($(Iter_Inp).val());
 

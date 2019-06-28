6f38cd621009708ef7b0645eaa63822e34371fa4
Jith Job <jithjob123@hotmail.com>
2019-06-28 15:45:30 +0530

    minor

--- wwwroot/js/FormBuilder/WebFormRenderer.js
+++ wwwroot/js/FormBuilder/WebFormRenderer.js
@@ -458,7 +458,7 @@ const WebFormRender = function (option) {
             return;
         $.each(this.FormObj.BeforeSaveRoutines.$values, function (k, r) {
             if (!r.IsDisabled && r.Script.Lang === 0 && r.Script.Code !== "") {
-                new Function("form", "user", `event`, atob(r.Script.Code)).bind("this-placeholder", this.setFormObject(), this.userObject)();
+                new Function("form", "user", `event`, atob(r.Script.Code)).bind(this.formObject, this.setFormObject(), this.userObject)();
             }
         }.bind(this));
     };

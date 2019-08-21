cf6d281ea1bd1a8918dcc077adfe440da4fb7b37
Dona Jose <donajose@expressbase.com>
2019-08-21 18:41:08 +0530

    Manageroles fix

--- Components/ManageRolesViewComponent.cs
+++ Components/ManageRolesViewComponent.cs
@@ -16,66 +16,74 @@ using ExpressBase.Common;
 
 namespace ExpressBase.Web.Components
 {
-	public class ManageRolesViewComponent : ViewComponent
-	{
-		protected JsonServiceClient ServiceClient { get; set; }
+    public class ManageRolesViewComponent : ViewComponent
+    {
+        protected JsonServiceClient ServiceClient { get; set; }
 
-		protected RedisClient Redis { get; set; }
+        protected RedisClient Redis { get; set; }
 
-		public ManageRolesViewComponent(IServiceClient _client, IRedisClient _redis)
-		{
-			this.ServiceClient = _client as JsonServiceClient;
-			this.Redis = _redis as RedisClient;
-		}
-		public async Task<IViewComponentResult> InvokeAsync(int itemid)
-		{
-			var fr = this.ServiceClient.Get<GetManageRolesResponse>(new GetManageRolesRequest { id = itemid, SolnId = ViewBag.cid });
-			ViewBag.AppCollection = EbSerializers.Json_Serialize(fr.ApplicationCollection);
-			ViewBag.SelectedRoleInfo = EbSerializers.Json_Serialize(fr.SelectedRoleInfo);
-			ViewBag.PermissionList = EbSerializers.Json_Serialize(fr.PermissionList);
-			ViewBag.RoleId = itemid;
-			TempData["_dict"] = GetPermissionOperationsAsJs();
-			foreach (var role in Enum.GetValues(typeof(SystemRoles)))
-			{
-				fr.RoleList.Add(new Eb_RoleObject() {
-					Name = role.ToString(),
-					Description = "SYSTEM ROLE",
-					Id = (int)role,
-					App_Id = -1,
-					Is_Anonymous = false,
-					Is_System = true
-				});
-			}
-			ViewBag.RoleList = EbSerializers.Json_Serialize(fr.RoleList);
-			ViewBag.Role2RoleList = EbSerializers.Json_Serialize(fr.Role2RoleList);
-			ViewBag.UsersList = EbSerializers.Json_Serialize(fr.UsersList);
-			ViewBag.LocationList = EbSerializers.Json_Serialize(fr.LocationList);
-			return View();
-		}
-		private string GetPermissionOperationsAsJs()
-		{
-			Assembly assembly = Assembly.GetAssembly(typeof(EbWebForm)); //DO NOT CHANGE
-			List<Eb_ObjectTypeOperations> _listObj = new List<Eb_ObjectTypeOperations>();
+        public ManageRolesViewComponent(IServiceClient _client, IRedisClient _redis)
+        {
+            this.ServiceClient = _client as JsonServiceClient;
+            this.Redis = _redis as RedisClient;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int itemid)
+        {
+            var fr = this.ServiceClient.Get<GetManageRolesResponse>(new GetManageRolesRequest { id = itemid, SolnId = ViewBag.cid });
+            ViewBag.AppCollection = EbSerializers.Json_Serialize(fr.ApplicationCollection);
+            ViewBag.SelectedRoleInfo = EbSerializers.Json_Serialize(fr.SelectedRoleInfo);
+            ViewBag.PermissionList = EbSerializers.Json_Serialize(fr.PermissionList);
+            ViewBag.RoleId = itemid;
+            TempData["_dict"] = GetPermissionOperationsAsJs();
+            foreach (var role in Enum.GetValues(typeof(SystemRoles)))
+            {
+                fr.RoleList.Add(new Eb_RoleObject()
+                {
+                    Name = role.ToString(),
+                    Description = "SYSTEM ROLE",
+                    Id = (int)role,
+                    App_Id = -1,
+                    Is_Anonymous = false,
+                    Is_System = true
+                });
+            }
+            ViewBag.RoleList = EbSerializers.Json_Serialize(fr.RoleList);
+            ViewBag.Role2RoleList = EbSerializers.Json_Serialize(fr.Role2RoleList);
+            ViewBag.UsersList = EbSerializers.Json_Serialize(fr.UsersList);
+            ViewBag.LocationList = EbSerializers.Json_Serialize(fr.LocationList);
+            return View();
+        }
+        private string GetPermissionOperationsAsJs()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(EbWebForm)); //DO NOT CHANGE
+            List<Eb_ObjectTypeOperations> _listObj = new List<Eb_ObjectTypeOperations>();
 
-			foreach (EbObjectType objectType in EbObjectTypes.Enumerator)
-			{
-				if (objectType.IsUserFacing)
-				{
-					var eOperations = assembly.GetType(string.Format("ExpressBase.Objects.Eb{0}", objectType.Name))
-					.GetField("Operations").GetValue(null);
+            foreach (EbObjectType objectType in EbObjectTypes.Enumerator)
+            {
+                if (objectType.IsUserFacing)
+                {
+                    try
+                    {
+                        var eOperations = assembly.GetType(string.Format("ExpressBase.Objects.Eb{0}", objectType.Name))
+                        .GetField("Operations").GetValue(null);
 
-					if (eOperations != null)
-					{
-						var _obj = new Eb_ObjectTypeOperations { Op_Id = objectType.IntCode, Op_Name = objectType.Name, Operations = new List<string>() };
-						foreach (var Op in (eOperations as EbOperations).Enumerator)
-							_obj.Operations.Add(Op.ToString());
+                        if (eOperations != null)
+                        {
+                            var _obj = new Eb_ObjectTypeOperations { Op_Id = objectType.IntCode, Op_Name = objectType.Name, Operations = new List<string>() };
+                            foreach (var Op in (eOperations as EbOperations).Enumerator)
+                                _obj.Operations.Add(Op.ToString());
 
-						_listObj.Add(_obj);
-					}
-				}
-			}
+                            _listObj.Add(_obj);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + "for" + objectType.Name + e.StackTrace);
+                    }
+                }
+            }
 
-			return EbSerializers.Json_Serialize(_listObj);
-		}
-	}
+            return EbSerializers.Json_Serialize(_listObj);
+        }
+    }
 }

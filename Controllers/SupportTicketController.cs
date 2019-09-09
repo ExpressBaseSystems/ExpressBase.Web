697074a87b03d41cde19fb043f80a21c5be548a4
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2019-09-09 11:40:26 +0530

    support ticket img upload

--- Controllers/SupportTicketController.cs
+++ Controllers/SupportTicketController.cs
@@ -11,6 +11,7 @@ using ServiceStack.Redis;
 using System.Net.Http.Formatting;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Specialized;
+using System.IO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,14 +23,15 @@ namespace ExpressBase.Web.Controllers
 		// GET: /<controller>/
 		public IActionResult bugsupport()
 		{
-			if (ViewBag.wc.Equals("tc")){
+			if (ViewBag.wc.Equals("tc"))
+			{
 
 				TenantSolutionsResponse ts = this.ServiceClient.Post<TenantSolutionsResponse>(new TenantSolutionsRequest { });
 				ViewBag.soluids = ts.soldispid;
 				ViewBag.solunames = ts.solname;
 				ViewBag.isolu = ts.solid;
 			}
-			FetchSupportResponse fsr = this.ServiceClient.Post<FetchSupportResponse>(new FetchSupportRequest{});
+			FetchSupportResponse fsr = this.ServiceClient.Post<FetchSupportResponse>(new FetchSupportRequest { });
 			ViewBag.tkttable = JsonConvert.SerializeObject(fsr);
 
 
@@ -38,8 +40,9 @@ namespace ExpressBase.Web.Controllers
 
 		public IActionResult EditTicket(string tktno)
 		{
-			SupportDetailsResponse sd = this.ServiceClient.Post<SupportDetailsResponse>(new SupportDetailsRequest {
-				ticketno=tktno
+			SupportDetailsResponse sd = this.ServiceClient.Post<SupportDetailsResponse>(new SupportDetailsRequest
+			{
+				ticketno = tktno
 			});
 			ViewBag.tktdetails = JsonConvert.SerializeObject(sd);
 			return View();
@@ -47,12 +50,12 @@ namespace ExpressBase.Web.Controllers
 
 		public IActionResult imgtest()
 		{
-			
+
 			return View();
 		}
 		public IActionResult imgtest1()
 		{
-			
+
 			return View();
 		}
 
@@ -60,33 +63,16 @@ namespace ExpressBase.Web.Controllers
 
 		[HttpPost]
 
-		public void testupload()
-		{
-			try
-			{
-				var req = this.HttpContext.Request.Form;
-				for (int i = 0; i < HttpContext.Request.Form.Files.Count; i++)
-				{
-					var file = HttpContext.Request.Form.Files[i];
-
-					//var fileName = Path.GetFileName(file.FileName);
-				}
-			}
-			catch(Exception e)
-			{
-
-			}
-			
-
-		}
+		
 
 
 
 
 
-		public void SaveBugDetails(string title,string descp,string priority,string solid,string type_f_b)
+		public void SaveBugDetails(string title, string descp, string priority, string solid, string type_f_b, object fileCollection)
 		{
 			string usrtyp = null;
+			SaveBugRequest sbrequest = new SaveBugRequest();
 
 			if (ViewBag.wc.Equals("dc"))
 			{
@@ -104,37 +90,43 @@ namespace ExpressBase.Web.Controllers
 			}
 			var httpreq = this.HttpContext.Request.Form;
 
+			byte[] fileData = null;
 			for (int i = 0; i < httpreq.Files.Count; i++)
 			{
 				var file = httpreq.Files[i];
 
-				//var fileName = Path.GetFileName(file.FileName);
+				using (var memoryStream = new MemoryStream())
+				{
+					file.CopyTo(memoryStream);
+					memoryStream.Seek(0, SeekOrigin.Begin);
+					fileData = new byte[memoryStream.Length];
+					memoryStream.ReadAsync(fileData, 0, fileData.Length);
+					sbrequest.Filecollection.Add(fileData);
+				}
 			}
+			sbrequest.title = httpreq["title"].ToString();
+			sbrequest.description = httpreq["descp"].ToString();
+			sbrequest.priority = httpreq["priority"].ToString();
+			sbrequest.solutionid = httpreq["solid"].ToString();
+			sbrequest.type_b_f = httpreq["type_f_b"].ToString();
+			sbrequest.status = "onhold";
+			sbrequest.usertype = usrtyp;
+			sbrequest.fullname = this.LoggedInUser.FullName;
+			sbrequest.email = this.LoggedInUser.Email;
 
-			SaveBugResponse sbr = this.ServiceClient.Post<SaveBugResponse>(new SaveBugRequest
-			{
-				title = httpreq["title"].ToString(),
-				description = httpreq["descp"].ToString(),
-				priority = httpreq["priority"].ToString(),
-				solutionid = httpreq["solid"].ToString(),
-				type_b_f = httpreq["type_f_b"].ToString(),
-				status = "onhold",
-				usertype = usrtyp,
-				fullname = this.LoggedInUser.FullName,
-				email = this.LoggedInUser.Email,
-				upload_files = httpreq.Files
-			}); ;
+
+			SaveBugResponse sbr = this.ServiceClient.Post<SaveBugResponse>(sbrequest);
 		}
 
 
-		public void UpdateTicket(string title, string descp, string priority,string tktid)
+		public void UpdateTicket(string title, string descp, string priority, string tktid)
 		{
 			UpdateTicketResponse upr = this.ServiceClient.Post<UpdateTicketResponse>(new UpdateTicketRequest
 			{
-				ticketid=tktid,
-				title=title,
-				description=descp,
-				priority=priority
+				ticketid = tktid,
+				title = title,
+				description = descp,
+				priority = priority
 			});
 		}
 

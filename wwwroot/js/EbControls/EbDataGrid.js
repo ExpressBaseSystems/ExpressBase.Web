e223d3dbfc293bbbddaaff7c0c30d04a0c637415
Jith Job <jithjob123@hotmail.com>
2019-10-03 16:52:07 +0530

    DG row delete

--- wwwroot/js/EbControls/EbDataGrid.js
+++ wwwroot/js/EbControls/EbDataGrid.js
@@ -378,9 +378,10 @@
         $.each(this.AllRowCtrls, function (rowId, inpCtrls) {
             if (parseInt(rowId) < 0 && $(`#${this.TableId} tbody tr[rowid=${rowId}]`).length === 0)// to skip newly added and then deleted rows
                 return true;
-            if ($(`#${this.TableId} tbody tr[rowid=${rowId}]`).attr("is-checked") === "true" || /* - if checked*/
-                $(`#${this.TableId} tbody tr[rowid=${rowId}]`).length === 0)// to manage deleted row
+            if ($(`#${this.TableId} tbody tr[rowid=${rowId}]`).attr("is-checked") === "true") /* - if checked*/
                 SingleTable.push(this.getRowWTs(rowId, inpCtrls));
+            else if ($(`#${this.TableId} tbody tr[rowid=${rowId}]`).length === 0)// to manage deleted row
+                SingleTable.push({ RowId: rowId, IsDelete: true });
         }.bind(this));
         console.log(SingleTable);
         return SingleTable;
@@ -1041,6 +1042,9 @@
         let rowId = $tr.attr("rowid");
         $tr.find("td *").hide(200);
         setTimeout(function () { $tr.remove(); }, 201);
+        if ($tr.attr("is-initialised") === 'false')
+            this.AllRowCtrls[rowId] = {};
+
         this.AllRowCtrls[rowId].IsDelete = true;
     };
 

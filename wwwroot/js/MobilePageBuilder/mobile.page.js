22aa48a2101be945e70861996ddd05316f21e4d3
Amal k dev <amaldev@expressbase.com>
2019-11-07 18:17:26 +0530

    mobile builder

---
+++ wwwroot/js/MobilePageBuilder/mobile.page.js
@@ -0,0 +1,38 @@
+﻿function EB_MobilePage(option) {
+    this.Config = $.extend({}, option);
+    this.validate = function () {
+        return true;
+    };
+
+    if (this.validate()) {
+        if (window.MobilePage === null || window.MobilePage === undefined)
+            window.MobilePage = {};
+        window.MobilePage["Tab" + this.Config.TabNum] = {};
+        window.MobilePage["Tab" + this.Config.TabNum].Constants = {};
+        window.MobilePage["Tab" + this.Config.TabNum].Creator = new EbMobStudio(this.Config);
+        return window.MobilePage["Tab" + this.Config.TabNum].Creator;
+    }
+    else {
+        console.log("initialization error");
+        return null;
+    }
+};
+
+function EbMobStudio(){
+    this.Conf = config;
+    this.EditObj = $.isEmptyObject(this.Conf.DsObj) ? null : this.Conf.DsObj;
+    this.EbObject = null;
+
+    this.pg = new Eb_PropertyGrid({
+        id: "eb_mobpage_properties" + this.Conf.TabNum,
+        wc: this.Conf.Wc,
+        cid: this.Conf.TenantId,
+        $extCont: $("#eb_mobpage_property_wrapr" + this.Conf.TabNum)
+    });
+
+    this.exe = function () {
+
+    };
+
+    this.exe();
+}

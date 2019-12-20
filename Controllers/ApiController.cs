66720146c3c426ac5559a0e152ed282dc1796007
Amal K Dev <amaldev@expressbase.com>
2019-12-20 18:33:00 +0530

    locations collections in auth api

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -20,6 +20,7 @@ using ExpressBase.Common.EbServiceStack.ReqNRes;
 using ExpressBase.Common.Enums;
 using Microsoft.Net.Http.Headers;
 using ExpressBase.Web.Filters;
+using ExpressBase.Common.LocationNSolution;
 
 namespace ExpressBase.Web.Controllers
 {
@@ -265,6 +266,18 @@ namespace ExpressBase.Web.Controllers
                     response.UserId = authResponse.User.UserId;
                     response.DisplayName = authResponse.User.FullName;
                     response.User = authResponse.User;
+
+                    Eb_Solution s_obj = this.Redis.Get<Eb_Solution>(String.Format("solution_{0}", this.SultionId));
+                    if (s_obj != null && authResponse.User.LocationIds != null)
+                    {
+                        foreach (int _locid in authResponse.User.LocationIds)
+                        {
+                            if (s_obj.Locations.ContainsKey(_locid))
+                            {
+                                response.Locations.Add(s_obj.Locations[_locid]);
+                            }
+                        }
+                    }
                 }
                 else
                     response.IsValid = false;

d87c4772b41d2e8f8445408b9ef34306f7953d94
Dona Jose <donajose@expressbase.com>
2020-01-02 18:54:18 +0530

    Scheduling sql job

--- Controllers/SqlJobController.cs
+++ Controllers/SqlJobController.cs
@@ -21,12 +21,12 @@ namespace ExpressBase.Web.Controllers
         {
             SqlJobResponse resp = this.ServiceClient.Post<SqlJobResponse>(new SqlJobRequest
             {
-                RefId= Refid,
+                RefId = Refid,
                 GlobalParams = new List<Param> { new Param { Name = "date_to_consolidate", Type = "6", Value = "28-02-2015" } }
             });
             return resp;
         }
-
+         
         public IActionResult SqlJobConsole()
         {
             List<int> types = new List<int>() { 26 };
@@ -34,28 +34,28 @@ namespace ExpressBase.Web.Controllers
             ViewBag.SqlJobObject = JsonConvert.SerializeObject(Result.Data);
             return View();
         }
-        public SqlJobsListGetResponse Get_Jobs_List(string Refid , string Date)
+        public SqlJobsListGetResponse Get_Jobs_List(string Refid, string Date)
         {
             SqlJobsListGetResponse resp = this.ServiceClient.Get(new SqlJobsListGetRequest()
             {
                 RefId = Refid,
-                Date =  Date
+                Date = Date
 
             });
-            var Temp = new DSController(this.ServiceClient , this.Redis);
+            var Temp = new DSController(this.ServiceClient, this.Redis);
             resp.SqlJobsDvColumns = EbSerializers.Json_Serialize(Temp.GetColumnsForSqlJob(resp.SqlJobsColumns));
-            return resp;  
+            return resp;
         }
 
         public RetryJobResponse JobRetry(int id)
         {
-            RetryJobResponse response = this.ServiceClient.Post<RetryJobResponse>(new RetryJobRequest { JoblogId = id , RefId = "ebdbllz23nkqd620180220120030-ebdbllz23nkqd620180220120030-26-2642-3506-2642-3506" });
+            RetryJobResponse response = this.ServiceClient.Post<RetryJobResponse>(new RetryJobRequest { JoblogId = id, RefId = "ebdbllz23nkqd620180220120030-ebdbllz23nkqd620180220120030-26-2642-3506-2642-3506" });
             return response;
         }
 
         public void ProcessorLogic()
         {
-            this.ServiceClient.Post <ProcessorResponse> (new ProcessorRequest()); ;
+            this.ServiceClient.Post<ProcessorResponse>(new ProcessorRequest()); ;
         }
     }
 }

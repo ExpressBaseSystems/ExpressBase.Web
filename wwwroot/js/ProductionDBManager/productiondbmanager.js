269845936073f7c12eeadf57ed70d301b07bd4fe
anoopa <anoopa.baby@expressbase.com>
2020-01-07 14:18:28 +0530

    ProductionDBManager - QueryCheck

--- wwwroot/js/ProductionDBManager/productiondbmanager.js
+++ wwwroot/js/ProductionDBManager/productiondbmanager.js
@@ -61,13 +61,16 @@
         //        dbdict[val] = data[val];
         //    }
         //}
-        if (data.length > 0) {
+        if (data.errorMessage != null) {
+            alert(data.errorMessage);
+        }
+        else if (data.changes.length > 0) {
             html = html + `
                                 <div class="row row-padding div-row-heading">
                                     <div class="col-md-4 ">
                                         <label class="sub-headings row-padding">
-                                                <a data-toggle="collapse" class="file_content_toggle" role="button" val="${val}sub" count-val = "${data.length}" style="text-decoration:none;color: #4987fb">
-                                                    ${data.length} Changes
+                                                <a data-toggle="collapse" class="file_content_toggle" role="button" val="${val}sub" count-val = "${data.changes.length}" style="text-decoration:none;color: #4987fb">
+                                                    ${data.changes.length} Changes
                                                     <i class="fa fa-chevron-down"></i>
                                                 </a>
                                         </label>
@@ -91,16 +94,16 @@
                                         </div>
                                     </div>
                                 </div>`;
-            $.each(data, function (i, vals) {
+            $.each(data.changes, function (i, vals) {
                 html = html + `<div class="row row-padding div-row-contents " id = "${val}${vals['fileHeader']}">
                                                         <div class="col-md-8">
                                                             <a data-target="#diffViewer" data-toggle="modal"  class="MainNavText" id="diffModal" 
        href="#diffViewer">
-                                                            <label class="table-content-font file_header_link" sol-id="${val}" file-name = "${vals['fileHeader']}" file-path="${vals['filePath']}" file-type="${vals['fileType']}" style="cursor: pointer;">${vals['fileHeader']}</label>
+                                                            <label class="table-content-link-font file_header_link" sol-id="${val}" file-name = "${vals['fileHeader']}" file-path="${vals['filePath']}" file-type="${vals['fileType']}" style="cursor: pointer;">${vals['fileHeader']}</label>
                                                              </a>
                                `;
                 if (vals['newItem'] == true) {
-                    html = html + `<label class="table-content-font" style="color: #4987fb;">New</label>`;
+                    html = html + `<label class="table-content-font" style="color: #162929;">New</label>`;
                 }
                 html = html + `   </div>
                                                         <div class="col-md-2">
@@ -109,13 +112,13 @@
                                                         <div class="col-md-2">
                                                             <input type="hidden" value="${val}" id="data_${val}" name="data"/>
                                                             <button data-toggle="modal"  type="button" id="${val}showquery" obj-val="${btoa(unescape(encodeURIComponent(JSON.stringify(vals))))}" sol-id="${val}" 
-                                                            class="btn btn-change btn-sm view_query" file-name = "${vals['fileHeader']}" id="${vals['fileHeader']}" data-loading-text="Getting Queries... <i class='fa fa-gear fa-spin' style='font-size:10px;margin-left:4px;'  ></i> " >Show Query</button>
+                                                            class="btn btn-change btn-sm view_query" file-type="${vals['fileType']}" file-name = "${vals['fileHeader']}" id="${vals['fileHeader']}" data-loading-text="Getting Queries... <i class='fa fa-gear fa-spin' style='font-size:10px;margin-left:4px;'  ></i> " >Show Query</button>
                                                         </div>
                                                     </div>`;
             });
             html = html + `</div>`;
         }
-        else if (data.length == 0) {
+        else if (data.changes.length == 0) {
             $(`#uptodate_${val}`).show();
             $(`#i_chk_${val}`).hide();
         }
@@ -126,14 +129,14 @@
         let changes = JSON.parse(decodeURIComponent(escape(window.atob(e.target.getAttribute("obj-val")))));
         let solution = e.target.getAttribute("sol-id");
         let key = $(e.target).siblings("input").val();
-        if (changes["type"] == "0") {
+        if (changes["type"] == "0" || changes["type"] == "2") {
             $.ajax({
                 type: "POST",
                 url: "../ProductionDBManager/GetFunctionOrProcedureQueries",
                 data: { change: changes, solution_id: solution },
                 success:
-                        this.AjaxSuccessFun.bind(this)
-                   
+                    this.AjaxSuccessFun.bind(this, changes)
+
             });
         }
         else if (changes["type"] == "1") {
@@ -142,22 +145,30 @@
                 url: "../ProductionDBManager/GetTableQueries",
                 data: { change: changes, solution_id: solution },
                 success:
-                        this.AjaxSuccessFun.bind(this)
+                    this.AjaxSuccessFun.bind(this, changes)
             });
         }
 
     }
 
-    this.AjaxSuccessFun = function (data) {
-        $('#viewQuery').modal({
-            show: true,
-            backdrop: "static"
-        });
-        this.CodeMirrorObject.setValue(data.query);
-        var that = this;
-        setTimeout(function () {
-            that.CodeMirrorObject.refresh();
-        }, 500);
+    this.AjaxSuccessFun = function (changes, data) {
+        if (data.errorMessage == null) {
+            $('#viewQuery').modal({
+                show: true,
+                backdrop: "static"
+            });
+            this.CodeMirrorObject.setValue(data.query);
+            var that = this;
+            setTimeout(function () {
+                that.CodeMirrorObject.refresh();
+            }, 500);
+        }
+        else {
+            if (data.errorMessage == "Execution Completed !!! Changes Found!!!") {
+                changes.newItem = false;
+            }
+            alert(data.errorMessage);
+        }
     };
 
     this.codemirroTrigger = function () {
@@ -172,39 +183,48 @@
 
     this.getquery = function () {
         let query = this.CodeMirrorObject.getValue();
+        let queryCheck = this.CodeMirrorObject.getValue().toUpperCase().replace("  "," ");
         let solution_id = $(`#solution_id`).val();
+        let filetype = $(`#file_type`).val();
         let filename = $(`#file_header`).val();
         let msg = "Contains ";
         let f = 0;
-        if (query.includes("DROP TABLE")) {
-            f = 1;
-            msg = msg + "\"DROP TABLE\"";
-        }
-        else if (query.includes("DROP COLUMN"))
-        {
-            f = 1;
-            msg = msg + " \"DROP COLUMN\"";
-        }
-        else if (query.includes("INSERT INTO"))
-        {
-            f = 1;
-            msg = msg + " \"INSERT\"";
-        }
-        else if (query.includes("UPDATE"))
-        {
-            f = 1;
-            msg = msg + " \"UPDATE\"";
-        }
-        else if (query.includes("DELETE FROM"))
-        {
-            f = 1;
-            msg = msg + " \"DELETE\"";
+        if (filetype == "Table") {
+            if (queryCheck.includes("DROP TABLE") && !queryCheck.includes("-- DROP TABLE")) {
+                f = 1;
+                msg = msg + "\"DROP TABLE\"";
+            }
+            else if (queryCheck.includes("DROP COLUMN")) {
+                f = 1;
+                msg = msg + " \"DROP COLUMN\"";
+            }
+            else if (queryCheck.includes("INSERT INTO")) {
+                f = 1;
+                msg = msg + " \"INSERT\"";
+            }
+            else if (queryCheck.includes("UPDATE")) {
+                f = 1;
+                msg = msg + " \"UPDATE\"";
+            }
+            else if (queryCheck.includes("DELETE FROM")) {
+                f = 1;
+                msg = msg + " \"DELETE\"";
+            }
+            else {
+                $.ajax({
+                    type: "POST",
+                    url: "../ProductionDBManager/ExecuteQuery",
+                    data: { query: query, solution_id: solution_id, filename: filename, filetype: filetype },
+                    success:
+                        this.getqueryAjaxSuccess.bind(this)
+                });
+            }
         }
         else {
             $.ajax({
                 type: "POST",
                 url: "../ProductionDBManager/ExecuteQuery",
-                data: { query: query, solution_id: solution_id },
+                data: { query: query, solution_id: solution_id, filename: filename, filetype: filetype },
                 success:
                     this.getqueryAjaxSuccess.bind(this)
             });
@@ -213,7 +233,7 @@
             msg = msg + ". Please Confirm Query...";
             this.clickEventFunction(msg);
         }
-       
+
     }
 
     this.clickEventFunction = function (msg) {
@@ -230,34 +250,48 @@
         });
     }
 
-    this.dialogboxAction = function(value){
-                $('#viewQuery').modal({
+    this.dialogboxAction = function (value) {
+        $('#viewQuery').modal({
             show: true,
             backdrop: "static"
         });
     }
 
-    this.getqueryAjaxSuccess = function () {
-        alert('Execution Completed !!!');
-        let x = this.currentDiv.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[0].children[0].getAttribute("count-val");
-        if (x == 1) {
-            let key = this.currentDiv.parentNode.parentNode.parentNode.parentNode.getAttribute("id");
-            this.currentDiv.parentNode.parentNode.parentNode.parentNode.remove();
-            var today = new Date();
-            var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            var dateTime = date + ' ' + time;
-            $(`#uptodate_${key}`).show();
-            $(`#i_chk_${key}`).hide();
-            $(`#${key}`).empty();
-            $(`#modified_date_${key}`).empty();
-            $(`#modified_date_${key}`).append(` <label class="sub-headings row-padding">${dateTime}</label>`);
+    this.getqueryAjaxSuccess = function (data) {
+        if (data.errorMessage == "") {
+            alert('Execution Completed !!!');
+            let x = this.currentDiv.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[0].children[0].getAttribute("count-val");
+            if (x == 1) {
+                let key = this.currentDiv.parentNode.parentNode.parentNode.parentNode.getAttribute("id");
+                this.currentDiv.parentNode.parentNode.parentNode.parentNode.remove();
+                var today = new Date();
+                var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                var dateTime = date + ' ' + time;
+                $(`#uptodate_${key}`).show();
+                $(`#i_chk_${key}`).hide();
+                $(`#${key}`).empty();
+                $(`#modified_date_${key}`).empty();
+                $(`#modified_date_${key}`).append(` <label class="sub-headings row-padding">${dateTime}</label>`);
+            }
+            else {
+                x = x - 1;
+                this.currentDiv.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[0].children[0].setAttribute("count-val", x);
+                let id = $(this.currentDiv).closest(".sol_changes").attr("id");
+                let div = $(`a[val="${id}sub"]`);
+                div.empty().append(`${x} Changes <i class="fa fa-chevron-down"></i>`);
+                this.currentDiv.parentElement.parentElement.remove();
+            }
         }
         else {
-            x = x - 1;
-            this.currentDiv.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[0].children[0].innerText = x + ` Changes`;
-            this.currentDiv.parentNode.parentNode.parentNode.parentNode.children[0].children[0].children[0].children[0].setAttribute("count-val", x); 
-            this.currentDiv.parentElement.parentElement.remove();
+            if (data.errorMessage == "Execution Completed !!! Changes Found!!!" ) {
+                let changes = JSON.parse(decodeURIComponent(escape(window.atob(this.currentDiv.getAttribute("obj-val")))));
+                if (changes.newItem == true && data.changes[0].newItem == false) {
+                    this.currentDiv.parentNode.previousElementSibling.previousElementSibling.children[1].remove();
+                }
+                this.currentDiv.setAttribute("obj-val", btoa(unescape(encodeURIComponent(JSON.stringify(data.changes[0])))));
+            }
+            alert(data.errorMessage);
         }
     }
 
@@ -267,9 +301,16 @@
         $.ajax({
             type: "POST",
             url: "../ProductionDBManager/UpdateInfraWithSqlScripts",
-            success: function () {
+            success: function (data) {
                 $this.button('reset');
-                alert('Success');
+                if (data.errorMessage == "") {
+                    alert('Success');
+                }
+                else {
+                    alert(data.errorMessage);
+                }
+                $("#eb_common_loader").EbLoader("show", { maskItem: { Id: "body" } })
+                window.location.reload();
             },
         });
     }
@@ -278,6 +319,7 @@
         let abc = e.target.getAttribute("sol-id");
         $("#solution_id").empty().val(abc);
         $("#file_header").empty().val(e.target.getAttribute("file-name"));
+        $("#file_type").empty().val(e.target.getAttribute("file-type"));
         this.currentDiv = e.target;
     };
 
@@ -296,9 +338,10 @@
         });
     };
 
-    this.showDiff = function (data ) {
+    this.showDiff = function (data) {
         $("#eb-loader-diff").EbLoader("hide");
-        $('#diff_result').append(`
+        if (data.errorMessage == null) {
+            $('#diff_result').append(`
             <div class="row diffHeader">
             <div class="col-md-6">Infra DB Content</div>
             <div class="col-md-6">Tenant DB Content</div> 
@@ -307,16 +350,21 @@
             </div> 
               <div id='newtext' class='rightPane'> 
             </div>`);
-        $('#oldtext').html(data["result"][0]);
-        $('#newtext').html(data["result"][1]);
-        $('.leftPane').scroll(function () {
-            $('.rightPane').scrollTop($(this).scrollTop());
-            $('.rightPane').scrollLeft($(this).scrollLeft());
-        });
-        $('.rightPane').scroll(function () {
-            $('.leftPane').scrollTop($(this).scrollTop());
-            $('.leftPane').scrollLeft($(this).scrollLeft());
-        });
+            $('#oldtext').html(data["result"][0]);
+            $('#newtext').html(data["result"][1]);
+            $('.leftPane').scroll(function () {
+                $('.rightPane').scrollTop($(this).scrollTop());
+                $('.rightPane').scrollLeft($(this).scrollLeft());
+            });
+            $('.rightPane').scroll(function () {
+                $('.leftPane').scrollTop($(this).scrollTop());
+                $('.leftPane').scrollLeft($(this).scrollLeft());
+            });
+        }
+        else {
+            $('#diffViewer').modal('hide');
+            alert(data.errorMessage);
+        }
     };
 
 

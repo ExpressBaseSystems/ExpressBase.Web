9ce777af75314804652f58bebfa7af674953158b
Kurian Cherian <kurianlive@LIVE.COM>
2020-01-14 11:31:27 +0530

    openstreetmaps

--- Controllers/ConnectionManagerController.cs
+++ Controllers/ConnectionManagerController.cs
@@ -667,6 +667,25 @@ namespace ExpressBase.Web.Controllers
                     Id = Convert.ToInt32(req["Id"])
                 };
             }
+            if (vendor == DatabaseVendors.MSSQL)
+            {
+                con = new MSSqlConfig()
+                {
+                    DatabaseName = req["databaseName"],
+                    Server = req["server"],
+                    Port = Convert.ToInt32(req["port"]),
+                    UserName = req["userName"],
+                    Password = req["password"],
+                    ReadWriteUserName = req["readWriteUserName"],
+                    ReadWritePassword = req["readWritePassword"],
+                    ReadOnlyUserName = req["readOnlyUserName"],
+                    ReadOnlyPassword = req["readOnlyPassword"],
+                    Timeout = Convert.ToInt32(req["timeout"]),
+                    IsSSL = (req["IsSSL"] == "on") ? true : false,
+                    NickName = req["nickname"],
+                    Id = Convert.ToInt32(req["Id"])
+                };
+            }
             try
             {
                 response = this.ServiceClient.Post<AddDBResponse>(new AddDBRequest
@@ -1186,7 +1205,7 @@ namespace ExpressBase.Web.Controllers
 
             try
             {
-                if (preferancetype != null)
+                if (preferancetype == string.Empty)
                 {
                     var req = JsonConvert.DeserializeObject<EbIntegration>(preferancetype);
                     res = this.ServiceClient.Post<EbIntegrationResponse>(new EbIntegrationRequest { IntegrationO = req, SolnId = sid });

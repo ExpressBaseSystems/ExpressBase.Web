c2fb026cb568745b6a0666c48f11431e1ed61145
Jith Job <jithjob@expressbase.com>
2020-01-17 18:41:01 +0530

    getBooleanDispMembrs() added, boolean column fixes, insertRowBelow fixes, insertRowAt fixes

--- wwwroot/js/EbControls/EbDataGrid.js
+++ wwwroot/js/EbControls/EbDataGrid.js
@@ -340,6 +340,13 @@
             return col.FalseText;
     };
 
+    this.getBooleanDispMembrs = function (cellObj, rowId, col) {
+        if (cellObj.Value === true)
+            return "✔";
+        else
+            return "✖";
+    };
+
     this.getDispMembr = function (inpCtrl) {
         let rowId = inpCtrl.__rowid;
         let cellObj = inpCtrl.DataVals;
@@ -357,6 +364,9 @@
         }
         else if (col.ObjType === "DGBooleanSelectColumn") {
             dspMmbr = this.getBSDispMembrs(cellObj, rowId, col);
+        }
+        else if (col.ObjType === "DGBooleanColumn") {
+            dspMmbr = this.getBooleanDispMembrs(cellObj, rowId, col);
         }
         else if (col.ObjType === "DGNumericColumn") {
             dspMmbr = cellObj.F || cellObj.Value || "0.00"; // temporary fix
@@ -724,8 +734,10 @@
     }.bind(this);
 
     this.insertRowAt = function (insertIdx, $tr) {
-        if (insertIdx > 2)
+        if (insertIdx < 1)
             $(`#${this.TableId}>tbody`).prepend($tr);
+        else if ($(`#${this.TableId}>tbody>tr`).length === insertIdx)
+            $tr.insertAfter($(`#${this.TableId}>tbody>tr:eq(${insertIdx - 1})`));
         else
             $tr.insertBefore($(`#${this.TableId}>tbody>tr:eq(${insertIdx})`));
 
@@ -1769,6 +1781,13 @@
     };
 
     this.insertRowBelow = function (eType, selector, action, originalEvent) {
+        let $activeRow = $(`#${this.TableId} tbody tr[is-editing="true"]`);
+        if ($activeRow.length === 1) {
+            if (this.RowRequired_valid_Check($activeRow.attr("rowid"))); {
+                let td = $activeRow.find('td:last')[0];
+                this.checkRow_click({ target: td }, false, false);
+            }
+        }
         let $e = selector.$trigger;
         let $tr = $e.closest("tr");
         this.addRow({ insertIdx: $tr.index() + 1 });

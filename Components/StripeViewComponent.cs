e3c929725202a15c5403cddede32cc51d29c8ad2
anoopa <anoopa.baby@expressbase.com>
2020-01-23 11:04:02 +0530

    Stripe and Env SE URL

--- Components/StripeViewComponent.cs
+++ Components/StripeViewComponent.cs
@@ -24,58 +24,66 @@ namespace ExpressBase.Web.Components
         }
         public async Task<IViewComponentResult> InvokeAsync(string sid)
         {
-            ViewBag.pb_key = Environment.GetEnvironmentVariable(EnvironmentConstants.EB_STRIPE_PUBLISHABLE_KEY);
-            Eb_Solution soln = this.Redis.Get<Eb_Solution>(String.Format("solution_{0}", sid));
-            string cust_id = "";
-            if (soln == null)
-            { 
-                this.ServiceClient.Post(new UpdateSolutionObjectRequest { SolnId = sid});
-                soln = this.Redis.Get<Eb_Solution>(String.Format("solution_{0}",sid));
-            }
-            ViewBag.MinUsers = soln.NumberOfUsers;
-            if (soln.PricingTier == 0)
+            try
             {
-                ViewBag.Status = true;
+                ViewBag.pb_key = Environment.GetEnvironmentVariable(EnvironmentConstants.EB_STRIPE_PUBLISHABLE_KEY);
+                Eb_Solution soln = this.Redis.Get<Eb_Solution>(String.Format("solution_{0}", sid));
+                string cust_id = "";
+                if (soln == null)
+                {
+                    this.ServiceClient.Post(new UpdateSolutionObjectRequest { SolnId = sid });
+                    soln = this.Redis.Get<Eb_Solution>(String.Format("solution_{0}", sid));
+                }
+                ViewBag.MinUsers = soln.NumberOfUsers;
+                if (soln.PricingTier == 0)
+                {
+                    ViewBag.Status = true;
+                }
+                else
+                {
+                    ViewBag.Status = false;
+                    //---------------------------Plan------------------------
+                    CheckCustomerSubscribedResponse cust1 = this.ServiceClient.Post<CheckCustomerSubscribedResponse>(new CheckCustomerSubscribedRequest
+                    {
+                        SolnId = sid
+                    });
+                    ViewBag.Plan = cust1.Plan;
+                    ViewBag.Users = cust1.Users;
+                    ViewBag.CustId = cust1.CustId;
+                    cust_id = cust1.CustId;
+                    //--------------------------Customer----------------------
+                    GetCustomerResponse cust = this.ServiceClient.Post<GetCustomerResponse>(new GetCustomerRequest
+                    {
+                        CustId = cust_id
+                    });
+                    ViewBag.Cust = cust;
+                    //-------------------------Card-------------------------------
+                    GetCardResponse resp = this.ServiceClient.Post<GetCardResponse>(new GetCardRequest
+                    {
+                        CustId = cust_id
+                    });
+                    ViewBag.Card = resp;
+
+                    //---------------------------Invoice----------------------
+                    GetCustomerInvoiceResponse stripeinvoice = this.ServiceClient.Post<GetCustomerInvoiceResponse>(new GetCustomerInvoiceRequest
+                    {
+                        CustId = cust_id,
+                    });
+                    ViewBag.Count = stripeinvoice.Invoices.List.Count;
+                    ViewBag.StripeInvoice = stripeinvoice;
+
+                    GetCustomerUpcomingInvoiceResponse stripeupcominginvoice = this.ServiceClient.Post<GetCustomerUpcomingInvoiceResponse>(new GetCustomerUpcomingInvoiceRequest
+                    {
+                        CustId = cust_id
+                    });
+                    ViewBag.UpCount = stripeupcominginvoice.Invoice.Data.Count;
+                    ViewBag.StripeUpcomingInvoice = stripeupcominginvoice;
+                }
             }
-            else
+            catch(Exception e)
             {
-                ViewBag.Status = false;
-                //---------------------------Plan------------------------
-                CheckCustomerSubscribedResponse cust1 = this.ServiceClient.Post<CheckCustomerSubscribedResponse>(new CheckCustomerSubscribedRequest
-                {
-                    SolnId = sid
-                });
-                ViewBag.Plan = cust1.Plan;
-                ViewBag.Users = cust1.Users;
-                ViewBag.CustId = cust1.CustId;
-                cust_id = cust1.CustId;
-                //--------------------------Customer----------------------
-                GetCustomerResponse cust = this.ServiceClient.Post<GetCustomerResponse>(new GetCustomerRequest
-                {
-                    CustId = cust_id
-                });
-                ViewBag.Cust = cust;
-                //-------------------------Card-------------------------------
-                GetCardResponse resp = this.ServiceClient.Post<GetCardResponse>(new GetCardRequest
-                {
-                    CustId = cust_id
-                });
-                ViewBag.Card = resp;
-
-                //---------------------------Invoice----------------------
-                GetCustomerInvoiceResponse stripeinvoice = this.ServiceClient.Post<GetCustomerInvoiceResponse>(new GetCustomerInvoiceRequest
-                {
-                    CustId = cust_id,
-                });
-                ViewBag.Count = stripeinvoice.Invoices.List.Count;
-                ViewBag.StripeInvoice = stripeinvoice;
-
-                GetCustomerUpcomingInvoiceResponse stripeupcominginvoice = this.ServiceClient.Post<GetCustomerUpcomingInvoiceResponse>(new GetCustomerUpcomingInvoiceRequest
-                {
-                    CustId = cust_id
-                });
-                ViewBag.UpCount = stripeupcominginvoice.Invoice.Data.Count;
-                ViewBag.StripeUpcomingInvoice = stripeupcominginvoice;
+                Console.WriteLine(e);
+                return View("StripeHome");
             }
             return View("StripeHome");
         }

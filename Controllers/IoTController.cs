5d71663a5a629f57499761b935851d1a264fe2fa
Dona Jose <donajose@expressbase.com>
2020-01-27 15:36:57 +0530

    IotController

---
+++ Controllers/IoTController.cs
@@ -0,0 +1,23 @@
+﻿using ExpressBase.Web.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Controllers
+{
+    public class IoTController : EbBaseExtController
+    {
+        public IoTController(IServiceClient _ssclient) : base(_ssclient) { }
+
+
+        [HttpPost]
+        public void PushData(string data)
+        {
+            Console.WriteLine("__________________________________________________Received Input Data : " + data);
+
+        }
+    }
+}

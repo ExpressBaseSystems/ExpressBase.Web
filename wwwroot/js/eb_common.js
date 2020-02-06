7f5c728510e4747649875f2e45590e5a107a6d77
Nithin <51478970+NithinVasudev@users.noreply.github.com>
2020-02-06 19:57:02 +0530

    Dashboard - Gauge Customization ,default gauge config

--- wwwroot/js/eb_common.js
+++ wwwroot/js/eb_common.js
@@ -1047,3 +1047,17 @@ function getEbFormatedPSRows(ctrl) {
     }
     return columnVals;
 }
+
+function GetFontCss(obj) {
+    let font = [];
+    font.push(`font-size:${obj.Size}px ;`);
+    font.push(`color:${obj.color} ;`);
+    font.push(`font-family:${obj.FontName} ;`);
+    if (font.Underline) { font.push(`text-decoration: underline ;`); }
+    if (font.Strikethrough) { font.push(`text-decoration: line-through ;`); }
+    if (font.Caps) { font.push(`text-transform: uppercase;`); }
+    if (font.Style === 1) { font.push(`font: bold;`); }
+    if (font.Style === 2) { font.push(`font: italic;`); }
+    if (font.Style === 3) { font.push(`font: italic bold;`); }
+    return (font.join().replace(/\,/g, ''));
+}

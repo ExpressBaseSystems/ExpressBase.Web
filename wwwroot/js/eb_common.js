f43dcea5c32883be071d44e9b5263b51fdd78c9c
Jith Job <jithjob@expressbase.com>
2020-02-19 19:55:50 +0530

    Form data clone. getUrlParameter Fn added

--- wwwroot/js/eb_common.js
+++ wwwroot/js/eb_common.js
@@ -1101,3 +1101,18 @@ function blink(el, delay = 1000) {
     else
         blink($(el), delay);
 }
+
+const getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};

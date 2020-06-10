08de4a86aeb706d3d8a967d47e2ff93a61fcc705
jith job <jithjob123@hotmail.com>
2020-06-10 16:48:44 +0530

    PG - clear all DependantProps after a dataSource changed

--- wwwroot/js/FormBuilder/PGHelper.js
+++ wwwroot/js/FormBuilder/PGHelper.js
@@ -8,7 +8,7 @@
             url: "../DS/GetColumns4Control",
             data: { DataSourceRefId: this.PGobj.PropsObj.DataSourceId },
             success: function (Columns) {
-                this.clearDependantProps("Columns");
+                this.clearDependantProps("Columns");// destination name hard coding
                 this.PGobj.PropsObj["Columns"] = JSON.parse(Columns);
                 this.PGobj.refresh();
                 $.LoadingOverlay('hide');
@@ -16,16 +16,22 @@
         });
     }.bind(this);
 
-    this.clearDependantProps = function (prop) {
-        let names = [];
-        this.PGobj.Metas.forEach(function (meta, i) {
-            if (meta.source === prop)
-                this.clearProp(meta);
-        }.bind(this));
+    this.clearDependantProps = function (propName) {
+        let CurMeta = getObjByval(this.PGobj.Metas, "name", propName)
+        this.clearProp(CurMeta);
+        if (CurMeta.dependentPropsList !== undefined) {
+            // loop through CurMeta.dependentPropsList
+            for (let i = 0; i < CurMeta.dependentPropsList.length; i++) {
+                let depPropName = CurMeta.dependentPropsList[i];
+                let depPropMeta = getObjByval(this.PGobj.Metas, "name", depPropName);
+                if (propName !== depPropName)
+                    this.clearDependantProps(depPropName);// and clear each props value if value changed
+            }
+        }
     };
 
     this.clearProp = function (meta) {
-        if (meta.editor === 24 || meta.editor === 8 && meta.Limit !== 1)
+        if (meta.editor === 24 ||meta.editor === 27 || (meta.editor === 8 && meta.Limit !== 1))
             this.PGobj.PropsObj[meta.name].$values = [];
         else if (meta.editor === 8 && meta.Limit === 1)
             this.PGobj.PropsObj[meta.name] = null;

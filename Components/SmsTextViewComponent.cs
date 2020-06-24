a52df90a01b854d10db352d1944ff1741424869f
Dona Jose <donajose@expressbase.com>
2020-06-24 15:17:14 +0530

    Minor - custom sms

---
+++ Components/SmsTextViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Components
+{
+    public class SmsTextViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("smsbody");
+        }
+    }
+}

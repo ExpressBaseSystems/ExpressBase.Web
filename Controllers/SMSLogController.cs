3e7641d3cf8844c8d78218bc8f0d2a6b931f12ad
JosevinCJ <josevin@expressbase.com>
2020-07-02 20:38:51 +0530

    SmsogConsole Fromdate & Todate added

--- Controllers/SMSLogController.cs
+++ Controllers/SMSLogController.cs
@@ -21,55 +21,10 @@ using Twilio.Rest.Api.V2010.Account;
 
 namespace ExpressBase.Web.Controllers
 {
-    public class SMSController : EbBaseIntCommonController
+    public class SMSLogController : EbBaseIntCommonController
     {
-        public SMSController(IServiceClient _ssclient, IRedisClient _redis) : base(_ssclient, _redis) { }
-
-        //[HttpGet]
-        //public IActionResult SendSMS()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //public IActionResult SendSMS(int i)
-        //{
-        //    var req = this.HttpContext.Request.Form;
-        //    SMSInitialRequest sMSSentRequest = new SMSInitialRequest();
-        //    //sMSSentRequest.To = req["to"];
-        //    //sMSSentRequest.From = req["from"];
-        //    //sMSSentRequest.Body = req["body"];
-        //    string[] subdomain = this.HttpContext.Request.Host.Host.Split('.');
-        //    sMSSentRequest.SolnId = subdomain[0];
-
-        //    var client = new JsonServiceClient(this.ServiceClient.BaseUri)
-        //    {
-        //        Credentials = new NetworkCredential("GATblcTqWNFI9ljZRWX-aUtidVYjJwoj", "")
-        //    };
-        //    client.Post(sMSSentRequest);
-        //    return View();
-        //}
-
-        //[HttpPost("smscallback/{apikey}")]
-        //public void CallBack(string apikey)
-        //{
-        //    var req = this.HttpContext.Request.Form;
-        //    var smsSid = Request.Form["SmsSid"];
-        //    var messageStatus = Request.Form["MessageStatus"];
-        //    string stsbody = req.ToJson();
-        //    SMSInitialRequest sMSSentRequest = new SMSInitialRequest();
-        //  //  sMSSentRequest.To = "+919961596200";
-        //  //  sMSSentRequest.Body = "SMS Id: "+smsSid.ToString()+"/nMessageStatus:" + messageStatus.ToString() +"/nFull Message:" + stsbody.ToString();
-        //    string[] subdomain = this.HttpContext.Request.Host.Host.Split('.');
-        //    sMSSentRequest.SolnId = subdomain[0];
-
-        //    var client = new JsonServiceClient(this.ServiceClient.BaseUri)
-        //    {
-        //        Credentials = new NetworkCredential(apikey, "")
-        //    };
-        //    client.Post(sMSSentRequest);
-        //}
-
+        public SMSLogController(IServiceClient _ssclient, IRedisClient _redis) : base(_ssclient, _redis) { }
+       
         public IActionResult SMSLogConsole()
         {
             Type[] typeArray = typeof(EbDashBoardWraper).GetTypeInfo().Assembly.GetTypes();
@@ -82,10 +37,10 @@ namespace ExpressBase.Web.Controllers
             List<int> types = new List<int>() { 19 };
             GetAllCommitedObjectsResp Result = this.ServiceClient.Get<GetAllCommitedObjectsResp>(new GetAllCommitedObjectsRqst { Typelist = types });
             ViewBag.SMSLogObject = JsonConvert.SerializeObject(Result.Data);
-            return View();
+            return View("SmsLogConsole");
         }
 
-        public ListSMSLogsResponse Get_SMS_List(string Refid, string Date)
+        public ListSMSLogsResponse Get_SMS_List(string Refid, string FromDate, string ToDate)
         {
             ListSMSLogsResponse resp = new ListSMSLogsResponse();
             string query = @"
@@ -95,14 +50,16 @@ namespace ExpressBase.Web.Controllers
 		                    eb_sms_logs l, eb_users u 
 	                    WHERE
 		                    u.id =l.eb_created_by AND
-		                    to_char(l.eb_created_at, 'dd-mm-yyyy') = :date AND l.refid = :refid AND 
-		                    COALESCE(l.eb_del,'F') = 'F'
+		                    (l.eb_created_at::date between :from_date AND :to_date) AND 
+							l.refid = :refid AND COALESCE(l.eb_del,'F') = 'F'
 	                    ORDER BY 
 		                    l.id DESC, status ASC;";
             List<Param> _params = new List<Param>();
-            _params.Add(new Param { Name = "date", Type = ((int)EbDbTypes.String).ToString(), Value = Date });
+            
+            _params.Add(new Param { Name = "from_date", Type = ((int)EbDbTypes.Date).ToString(), Value = FromDate });
+            _params.Add(new Param { Name = "to_date", Type = ((int)EbDbTypes.Date).ToString(), Value = ToDate });
             _params.Add(new Param { Name = "refid", Type = ((int)EbDbTypes.String).ToString(), Value = Refid });
-            string[] arrayy = new string[] { "send_from", "send_to", "message_body", "executed_by", "executed_at", "status", "id" };
+            string[] arrayy = new string[] { "From", "To", "Message", "Executed By", "Executed At", "Status", "id" };
             DVColumnCollection DVColumnCollection = GetColumnsForSMSLog(arrayy);
             EbDataVisualization Visualization = new EbTableVisualization { Sql = query, ParamsList = _params, Columns = DVColumnCollection, AutoGen = false, IsPaging = true };
             
@@ -119,17 +76,17 @@ namespace ExpressBase.Web.Controllers
                 foreach (string str in strArray)
                 {
                     DVBaseColumn _col = null;
-                    if (str == "send_from")
+                    if (str == "From")
                         _col = new DVStringColumn { Data = 0, Name = str, sTitle = str, Type = EbDbTypes.String, bVisible = true };
-                    if (str == "send_to")
+                    if (str == "To")
                         _col = new DVStringColumn { Data = 1, Name = str, sTitle = str, Type = EbDbTypes.String, bVisible = true };
-                    if (str == "message_body")
+                    if (str == "Message")
                         _col = new DVStringColumn { Data = 2, Name = str, sTitle = str, Type = EbDbTypes.String, AllowedCharacterLength = 20, bVisible = true };
-                    if (str == "executed_by")
+                    if (str == "Executed By")
                         _col = new DVStringColumn { Data = 3, Name = str, sTitle = str, Type = EbDbTypes.String, bVisible = true };
-                    if (str == "executed_at")
+                    if (str == "Executed At")
                         _col = new DVDateTimeColumn { Data = 4, Name = str, sTitle = str, Type = EbDbTypes.Date, bVisible = true, Format = DateFormat.DateTime };
-                    if (str == "status")
+                    if (str == "Status")
                         _col = new DVStringColumn { Data = 5, Name = str, sTitle = str, Type = EbDbTypes.String, bVisible = true };
                     if (str == "id")
                         _col = new DVNumericColumn { Data = 6, Name = str, sTitle = str, Type = EbDbTypes.Int32, bVisible = false };                   

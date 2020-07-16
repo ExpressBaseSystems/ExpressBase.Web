cfd550142fdf3811e2ac69ba8005850ee40566c8
jith job <jithjob123@hotmail.com>
2020-07-16 12:10:43 +0530

    followup

--- wwwroot/js/FormBuilder/FormRenderCommon.js
+++ wwwroot/js/FormBuilder/FormRenderCommon.js
@@ -549,8 +549,7 @@
             if (Validator.IsDisabled || !Validator.Script.Code)// continue; from loop if current validation IsDisabled
                 return true;
             let func = new Function('form', 'user', `event`, atob(Validator.Script.Code)).bind(ctrl, this.FO.formObject, this.FO.userObject);
-            this.updateFormValues();
-            let valRes = func(this.FO.formValues, this.FO.userObject);
+            let valRes = func();
             if (valRes === false) {
                 if (!Validator.IsWarningOnly) {
                     color = "#aa0000";
@@ -577,7 +576,7 @@
             if (Validator.IsDisabled || !Validator.Script.Code)// continue; from loop if current validation IsDisabled
                 return true;
             let func = new Function('form', 'user', `event`, atob(Validator.Script.Code)).bind(ctrl, this.FO.formObject, this.FO.userObject);
-            let valRes = func(this.FO.formValues, this.FO.userObject);
+            let valRes = func();
             if (valRes === false) {
                 //EbMakeInvalid(`#cont_${ctrl.EbSid_CtxId}`, `#${ctrl.EbSid_CtxId}Wraper`, Validator.FailureMSG, Validator.IsWarningOnly ? "warning" : "danger");
                 this.addInvalidStyle(ctrl, Validator.FailureMSG, (Validator.IsWarningOnly ? "warning" : "danger"));

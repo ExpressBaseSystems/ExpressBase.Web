e6f9f17cbafd2d1d27495aee91819b1ba9bdeb8f
Febin Carlos <febincarlos@expressbase.com>
2020-07-17 19:52:58 +0530

    onRowPaintFn called from confirmRow

--- wwwroot/js/EbControls/EbDataGrid.js
+++ wwwroot/js/EbControls/EbDataGrid.js
@@ -1057,6 +1057,7 @@
         this.ctrlToSpan_row(rowId);
         $activeTr.attr("is-checked", "true").attr("is-editing", "false");
         $(`#${this.TableId}>tbody>[is-editing=true]:first *:input[type!=hidden]:first`).focus();
+        this.onRowPaintFn($tr, "check", e);
         return true;
 
     };

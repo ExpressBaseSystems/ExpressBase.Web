f64900e1980ef4fcb9d516d8cfb162ff9f6533b2
jith job <jithjob123@hotmail.com>
2020-08-05 14:42:24 +0530

    PG url property URL value validation and message added

--- wwwroot/js/eb_common.js
+++ wwwroot/js/eb_common.js
@@ -1218,6 +1218,16 @@ function EbvalidateEmail(email) {
     return EbIsEmailOK(email);
 }
 
+function EbIsValidURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
+
 //function EbfixTrailingZeros(val, decLen) {
 //    //val = val.toString();
 //    //if (decLen <= 0)

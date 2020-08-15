34481e8a48f132f5d531846538eee018d8d75b48
jith job <jithjob123@hotmail.com>
2020-08-15 22:17:25 +0530

    Review Control Comment style aaded, Table style minor

--- wwwroot/js/EbControls/EbReview.js
+++ wwwroot/js/EbControls/EbReview.js
@@ -32,24 +32,24 @@
         this.$container.show();
     };
 
-    this.setRowData = function ($row, latestRowData) {
-        $.each(latestRowData, function (i, cell) {
-            if (cell.Name === "status")
-                $row.find("[col='status'] .selectpicker").selectpicker('val', cell.Value);
-            else if (cell.Name === "remarks")
-                $row.find("[col='remarks'] .fs-textarea").val(cell.Value);
-            else if (cell.Name === "eb_created_at") {
-                $row.find("[col='review-dtls'] .fs-time").text(cell.Value);
-            }
-            else if (cell.Name === "eb_created_by_name") {
-                $row.find("[col='review-dtls'] .fs-uname").text(cell.Value);
-            }
-            else if (cell.Name === "eb_created_by_id") {
-                let url = `url(../images/dp/${cell.Value}.png), url(../images/nulldp.png)`;
-                $row.find("[col='review-dtls'] .fs-dp").css("background-image", url);
-            }
-        }.bind(this));
-    };
+    //this.setRowData = function ($row, latestRowData) {
+    //    $.each(latestRowData, function (i, cell) {
+    //        if (cell.Name === "status")
+    //            $row.find("[col='status'] .selectpicker").selectpicker('val', cell.Value);
+    //        else if (cell.Name === "remarks")
+    //            $row.find("[col='remarks'] .fs-textarea").val(cell.Value);
+    //        else if (cell.Name === "eb_created_at") {
+    //            $row.find("[col='review-dtls'] .fs-time").text(cell.Value);
+    //        }
+    //        else if (cell.Name === "eb_created_by_name") {
+    //            $row.find("[col='review-dtls'] .fs-uname").text(cell.Value);
+    //        }
+    //        else if (cell.Name === "eb_created_by_id") {
+    //            let url = `url(../images/dp/${cell.Value}.png), url(../images/nulldp.png)`;
+    //            $row.find("[col='review-dtls'] .fs-dp").css("background-image", url);
+    //        }
+    //    }.bind(this));
+    //};
 
     //this.ctrl.ChangedRowObject = function () {
     //    return this.changedRowWT();
@@ -63,8 +63,17 @@
     };
 
     this.getCurRowValues = function () {
-        let action_unique_id = this.$curActiveRow.find("[col='status'] .selectpicker").selectpicker('val');
-        let comments = this.$curActiveRow.find("[col='remarks'] .fs-textarea").val();
+        let action_unique_id, comments;
+
+
+        if (this.ctrl.RenderAsTable) {
+            action_unique_id = this.$curActiveRow.find("[col='status'] .selectpicker").selectpicker('val');
+            comments = this.$curActiveRow.find("[col='remarks'] .fs-textarea").val();
+        }
+        else {
+            action_unique_id = this.$container.find(".rc-inp-cont .selectpicker").selectpicker('val');
+            comments = this.$container.find(".rc-inp-cont .rc-txtarea").val();
+        }
 
         for (let j = 0; j < this.CurStageDATA.Columns.length; j++) {
             let column = this.CurStageDATA.Columns[j];
@@ -155,7 +164,10 @@
     };
 
     this.switch2editMode = function () {
-        this.disableAllCtrls();
+        if (this.ctrl.RenderAsTable)
+            this.disableAllCtrls();
+        else
+            this.$container.find(".rc-inp-cont").hide();
     };
 
     this.switch2viewMode = function (DataMODEL) {
@@ -183,29 +195,73 @@
         });
     }.bind(this);
 
-    this.disableAllCtrls = function () {
-        $('.selectpicker').selectpicker();
-        this.$table.find(".fstd-div .fs-textarea").attr('disabled', 'disabled').css('pointer-events', 'none');
-        this.$table.find("td[col='status'] .dropdown-toggle").attr('disabled', 'disabled').css('pointer-events', 'none').find(".bs-caret").hide();
-        this.$submit.hide();
-        this.$container.find("tr").attr("active", "false");
+    //this.appendChatInpSection = function () {
+    //    if (!this.CurStageDATA || !this.hasPermission)
+    //        return;
+    //    this.$container.find(".rc-action-dd-wrap").append(this.curDDHtml);
+    //    this.$container.find(`.rc-action-dd-wrap .selectpicker`).selectpicker('val', this.curDDval);
+    //};
+
+    this.drawCommentBox = function () {
+        for (let i = 0; i < this.DataMODEL.length; i++) {
+            let row = this.DataMODEL[i];
+            let ebsid = getObjByval(row.Columns, "Name", "stage_unique_id").Value;
+            let stage = getObjByval(this.stages, "EbSid", ebsid);
+            let html = stage.Html;
+
+            html = html.replace("@rowid@", row.RowId);
+            html = html.replace("@slno@", i + 1);
+
+            for (let j = 0; j < row.Columns.length; j++) {
+                let column = row.Columns[j];
+                if (column.Name === "eb_created_by") {
+                    let userId = column.Value.split("$$")[0];
+                    let userName = column.Value.split("$$")[1] || "-------";
+                    let url = `url(../images/dp/${userId}.png), url(../images/nulldp.png)`;
+                    if (!userId)
+                        url = `url(../images/proimg.jpg)`;
+                    html = html.replace("@dpstyle@", `style='background-image:${url}'`)
+                        .replace("@uname@", userName);
+
+                    if (row.RowId === 0)
+                        this.$curDP = $(html).find(".fs-dp").clone();
+                }
+                if (column.Name === "action_unique_id") {
+                    if (row.RowId === 0) {
+
+                        this.$container.find(".rc-action-dd-wrap").append(stage.DDHtml);
+                        this.$container.find(`.rc-action-dd-wrap .selectpicker`).selectpicker('val', column.Value);
+                        //this.curDDHtml = stage.DDHtml;
+                        //this.curDDval = column.Value;
+                    }
+
+
+                    html = html.replace("@action@", getObjByval(stage.StageActions.$values, "EbSid", column.Value).Name);
+                    column.Value
+                }
+                else if (column.Name === "eb_created_at") {
+                    let dateString = column.Value,
+                        dateTimeParts = dateString.split(' '),
+                        timeParts = dateTimeParts[1].split(':'),
+                        dateParts = dateTimeParts[0].split('-'),
+                        date;
+
+                    date = new Date(dateParts[0], parseInt(dateParts[1], 10) - 1, dateParts[2], timeParts[0], timeParts[1], timeParts[2]);
+                    html = html.replace("@timeTitle@", column.Value || "").replace("@time@", column.Value ? timeDifference(Date.now(), date.getTime()) : "--/--/----");
+                }
+                else if (column.Name === "comments") {
+                    html = html.replace("@comment@", column.Value || '');
+                }
+            }
+            this.$container.find(".rc-msg-box").prepend(html);
+        }
+
+        if (!this.hasPermission) {
+            this.$container.find(".message[rowid='0'] .msg-comment").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Stage in Processing...<b>");
+            this.$submit.hide();
+        }
+
     };
-
-    this.enableRow = function () {
-        let $row = this.$tableBody.find("tr[rowid=0]");
-        this.$curActiveRow = $row;
-        if ($row.length === 0)
-            return;
-
-        if (!this.CurStageDATA || !this.hasPermission)
-            return;
-
-        $row.find(".fstd-div .fs-textarea").prop('disabled', false).css('pointer-events', 'inherit');
-        $row.find("td[col='status'] .dropdown-toggle").prop('disabled', false).css('pointer-events', 'inherit').find(".bs-caret").show();
-        this.$submit.show(300);
-        $row.attr("active", "true");
-    };
-
 
     this.drawTable = function () {
         this.$tableBody.empty();
@@ -218,6 +274,7 @@
 
             html = html.replace("@rowid@", row.RowId);
             html = html.replace("@slno@", i + 1);
+
             for (let j = 0; j < row.Columns.length; j++) {
                 let column = row.Columns[j];
                 if (column.Name === "eb_created_by") {
@@ -234,9 +291,6 @@
                         this.$tableBody.find(`tr[rowid='${row.RowId}'] [col='status'] .selectpicker`).selectpicker('val', column.Value);
                     }.bind(this));
                 }
-                //else if (column.Name === "eb_created_by") {
-                //    html = html.replace("@uname@", column.Value);
-                //}
                 else if (column.Name === "eb_created_at") {
                     html = html.replace("@time@", column.Value || "--/--/----");
                 }
@@ -246,9 +300,8 @@
             }
             this.$tableBody.append(html);
             this.runAfterRenderFuncs();
-            //let $html = $(html);
-            //this.$tableBody.append($html);
         }
+
         if (!this.hasPermission) {
             this.$tableBody.find("tr[rowid='0'] [col='review-dtls']").remove();
             this.$tableBody.find("tr[rowid='0'] [col='remarks']").remove();
@@ -270,18 +323,60 @@
         }
     };
 
+    this.disableAllCtrls = function () {
+        $('.selectpicker').selectpicker();
+        this.$table.find(".fstd-div .fs-textarea").attr('disabled', 'disabled').css('pointer-events', 'none');
+        this.$table.find("td[col='status'] .dropdown-toggle").attr('disabled', 'disabled').css('pointer-events', 'none').find(".bs-caret").hide();
+        this.$submit.hide();
+        this.$container.find("tr").attr("active", "false");
+    };
+
+    this.enableRow = function () {
+        let $row = this.$tableBody.find("tr[rowid=0]");
+        this.$curActiveRow = $row;
+        if ($row.length === 0)
+            return;
+
+        if (!this.CurStageDATA || !this.hasPermission)
+            return;
+
+        $row.find(".fstd-div .fs-textarea").prop('disabled', false).css('pointer-events', 'inherit');
+        $row.find("td[col='status'] .dropdown-toggle").prop('disabled', false).css('pointer-events', 'inherit').find(".bs-caret").show();
+        $row.attr("active", "true");
+    };
+
     this.set = function () {
         this.CurStageDATA = false;
         this.CurStageDATA = getObjByval(this.DataMODEL, "RowId", 0);
         this.isFormDataEditable = false;
         if (this.CurStageDATA) {
-            this.hasPermission = getObjByval(this.CurStageDATA.Columns, "Name", "has_permission").Value === "T";
+            this.hasPermission = getObjByval(this.CurStageDATA.Columns, "Name", "has_permission").Value === "T";//false;//
             this.isFormDataEditable = getObjByval(this.CurStageDATA.Columns, "Name", "is_form_data_editable").Value === "T";
         }
-        this.drawTable();
-        this.disableAllCtrls();
-        if (this.CurStageDATA)
-            this.enableRow();
+
+
+        if (this.ctrl.RenderAsTable) {
+            this.drawTable();
+            this.disableAllCtrls();
+            if (this.CurStageDATA)
+                this.enableRow();
+        }
+        else {
+            this.drawCommentBox();
+            this.$container.find(".message[rowid='0']").show();
+            this.$container.find(".rc-inp-cont").hide();
+
+            if (!this.CurStageDATA || !this.hasPermission)
+                return;
+
+            this.$container.find(".message[rowid='0']").hide();
+            this.$container.find(".rc-inp-cont").show().find(".rc-action-dp-wrap").prepend(this.$curDP);
+        }
+
+        if (!this.CurStageDATA || !this.hasPermission)
+            return;
+
+        this.$submit.show(300);
     };
 
     this.init();

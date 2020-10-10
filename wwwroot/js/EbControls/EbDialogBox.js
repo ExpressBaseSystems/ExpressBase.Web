4acecdd7cf734b45d30a31e70d942e906a3fbfb4
Amal K Dev <amaldev@expressbase.com>
2020-10-10 12:23:24 +0530

    user input in dialog box

--- wwwroot/js/EbControls/EbDialogBox.js
+++ wwwroot/js/EbControls/EbDialogBox.js
@@ -4,18 +4,21 @@
         Message: "Nothing specified",
         Buttons: {
             "Ok": {
+                ClassName: "eb_dlogBox_defaultbutton",
                 Background: "#508bf9",
                 Align: "left",
-                FontColor: "white;"
+                FontColor: "#ffffff"
             },
             "Cancel": {
-                Background: "#989898",
+                ClassName: "eb_dlogBox_defaultbutton default-cancel",
+                Background: "#ffffff",
                 Align: "right",
-                FontColor: "white"
+                FontColor: "#222222"
             }
         },
+        IsPrompt: false,
         hideClose: false,
-        CallBack: function (name) { }
+        CallBack: function (name, prompt) { console.log(name + ":" +prompt); }
     }, options);
 
     function div() {
@@ -24,22 +27,41 @@
                                 <div class="eb_dlogBox_container" id="eb_dlogBox_container">
                                     <div class="cw">
                                         @close@
-                                        <div class="msgbdy">${settings.Message}</div>
+                                        <div class="msgbdy">${setMessage()}</div>
                                         <div class="cnfrmBox-btnc">
                                             ${generateBtn()}
                                         </div>
                                     </div>
                                 </div>`.replace("@close@", settings.hideClose ? '' : '<div class="_cls"><i class="fa fa-close"></i></div>'));
         else {
-            $(`#eb_dlogBox_container .msgbdy`).html(settings.Message);
+            $(`#eb_dlogBox_container .msgbdy`).html(setMessage());
             $(`#eb_dlogBox_container .cnfrmBox-btnc`).html(generateBtn());
         }
+
         $(".dlgBoxBtn-cust").off("click").on("click", function (ev) {
+
             let n = $(ev.target).attr("name");
-            settings.CallBack(n);
+            let prompt = settings.IsPrompt ? $("#eb_dlogBox_prompt_input").val() : undefined;
+
+            settings.CallBack(n, prompt);
             hideMsg();
         });
-        $("#eb_dlogBox_container ._cls").on("click", function () { settings.CallBack("close"); hideMsg(); });
+
+        $("#eb_dlogBox_container ._cls").on("click", function () {
+            settings.CallBack("close");
+            hideMsg();
+        });
+    }
+
+    function setMessage() {
+        if (settings.IsPrompt) {
+            return `<div class="eb_dlogBox_prompt_inputContainer">
+                        <label class="eb_dlogBox_prompt_label">${settings.Message}</label>
+                        <input type="text" id="eb_dlogBox_prompt_input" class="eb_dlogBox_prompt_input"/>
+                    </div>`;
+        }
+        else
+            return settings.Message;
     }
 
     function showMsg() {
@@ -58,7 +80,10 @@
     function generateBtn() {
         let html = [];
         for (let key in settings.Buttons) {
-            html.push(`<button name="${key}" class="btn dlgBoxBtn-cust pull-${settings.Buttons[key].Align}"
+
+            let classname = settings.Buttons[key].ClassName || "";
+
+            html.push(`<button name="${key}" class="btn dlgBoxBtn-cust ${classname} pull-${settings.Buttons[key].Align}"
                 style="background:${settings.Buttons[key].Background};color:${settings.Buttons[key].FontColor}">${key}</button>`);
         }
         return html.join(" ");

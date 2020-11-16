0e336df7bfaac9b41987813a2e7302fe3fc3c746
Nithin <51478970+NithinVasudev@users.noreply.github.com>
2020-11-16 12:37:11 +0530

    Audio Control

---
+++ wwwroot/js/FormBuilder/AudioInput.js
@@ -0,0 +1,59 @@
+﻿var AudioInput = function (ctrl, ctrlopts) {
+    this.ctrl = ctrl;
+    this.ctrlopts = ctrlopts;
+
+    let audioIN = { audio: true };
+    let dataArray = [];
+    var mediaRecorder;
+    let playAudio = document.getElementById('adioPlay');
+    // audio is true, for recording 
+
+    // Access the permission for use 
+    // the microphone 
+
+
+    this.StartRec = function () {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+                mediaRecorder.addEventListener("dataavailable", event => {
+                    dataArray.push(event.data);
+                });
+                mediaRecorder.addEventListener("stop", () => {
+                    const audioBlob = new Blob(dataArray, { 'type': 'audio/mp3;' });
+                    const audioUrl = URL.createObjectURL(audioBlob);
+                    let playAudio = document.getElementById('adioPlay');
+
+                    playAudio.src = audioUrl;
+                    //dataArray = [];
+                    //const audio = new Audio(audioUrl);
+                    //audio.play();
+                });
+            });
+
+    };
+
+    this.StopRec = function () {
+        if (mediaRecorder) {
+            mediaRecorder.stop();
+        }
+    };
+    this.UploadAudio = function () {
+        let audioData = new Blob(dataArray, { 'type': 'audio/mp3;' });
+        var arrayBuffer;
+        var fileReader = new FileReader();
+        fileReader.onload = function (event) {
+            arrayBuffer = event.target.result;  
+        };
+        fileReader.readAsArrayBuffer(audioData);
+    };
+    this.init = function () {
+        $('#btnStart').on('click', this.StartRec.bind(this));
+        $('#btnStop').on('click', this.StopRec.bind(this));
+        $('#uploadAudio').on('click', this.UploadAudio.bind(this));
+    };
+
+    this.init();
+
+}

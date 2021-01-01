a2dbfc73f7b320f9a92ed392994ffda9a72c6ffb
Amal K Dev <amaldev@expressbase.com>
2021-01-01 12:07:39 +0530

    api v2, restsharp

---
+++ Controllers/Api/ApiV2Controller.cs
@@ -0,0 +1,27 @@
+﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+using ExpressBase.Web.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack;
+using ServiceStack.Redis;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace ExpressBase.Web.Controllers
+{
+    [Route("api/v2")]
+    public class ApiV2Controller : EbBaseIntApiController
+    {
+        public ApiV2Controller(IServiceClient _client, IRedisClient _redis) : base(_client, _redis) { }
+
+        [Route("data/visualization")]
+        public ActionResult<MobileDataResponse> GetMobileData(string refid, int limit, int offset, string param, string sort_order, string search)
+        {
+            if (!Authenticated)
+            {
+                return Unauthorized();
+            }
+
+
+            return null;
+        }
+    }
+}

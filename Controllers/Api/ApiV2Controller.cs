3091efa7cbc46a7d2b41d13fc8dd8ea28d9d9031
Amal K Dev <amaldev@expressbase.com>
2021-01-04 16:46:52 +0530

    approval button

--- Controllers/Api/ApiV2Controller.cs
+++ Controllers/Api/ApiV2Controller.cs
@@ -1,8 +1,12 @@
-﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+﻿using ExpressBase.Common.Data;
+using ExpressBase.Objects.ServiceStack_Artifacts;
 using ExpressBase.Web.BaseControllers;
+using ExpressBase.Web.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ServiceStack;
-using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace ExpressBase.Web.Controllers
@@ -10,18 +14,38 @@ namespace ExpressBase.Web.Controllers
     [Route("api/v2")]
     public class ApiV2Controller : EbBaseIntApiController
     {
-        public ApiV2Controller(IServiceClient _client, IRedisClient _redis) : base(_client, _redis) { }
+        public ApiV2Controller(IServiceClient _client) : base(_client) { }
 
         [Route("data/visualization")]
+        [EbApiAuthGaurd]
         public ActionResult<MobileDataResponse> GetMobileData(string refid, int limit, int offset, string param, string sort_order, string search)
         {
-            if (!Authenticated)
+            if (string.IsNullOrEmpty(refid)) return BadRequest("refid must be set");
+
+            try
             {
-                return Unauthorized();
+                EbMobileDataRequest request = new EbMobileDataRequest
+                {
+                    RefId = refid,
+                    Limit = limit,
+                    Offset = offset
+                };
+
+                if (param != null)
+                    request.Parameters.AddRange(JsonConvert.DeserializeObject<List<Param>>(param));
+
+                if (sort_order != null)
+                    request.SortColumns.AddRange(JsonConvert.DeserializeObject<List<SortColumn>>(sort_order));
+
+                if (search != null)
+                    request.SearchColumns.AddRange(JsonConvert.DeserializeObject<List<Param>>(search));
+
+                return ServiceClient.Get(request);
             }
-
-
-            return null;
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }

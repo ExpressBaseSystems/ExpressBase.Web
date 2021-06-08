3d00eaac1c626646a6acb97b5e554eb2635bf419
Febin Carlos <febincarlos@expressbase.com>
2021-06-08 16:27:33 +0400

    Starup, Program - minor

--- Program.cs
+++ Program.cs
@@ -7,22 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace ExpressBase.Web2
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var host = new WebHostBuilder()
-				.UseKestrel(options => {
-					options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
-					options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(5);
-					options.Limits.MinResponseDataRate = null;
-				})
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.UseUrls("http://*:41500/", "https://*:41502/")
-				//.UseUrls(urls: "http://*:41500/")
-				.UseStartup<Startup>()
-				.Build();
-			host.Run();
-		}
-	}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
+                    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(5);
+                    options.Limits.MinResponseDataRate = null;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseUrls("http://*:41500/", "https://*:41502/")
+                .UseUrls(urls: "http://*:41500/")
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
 }

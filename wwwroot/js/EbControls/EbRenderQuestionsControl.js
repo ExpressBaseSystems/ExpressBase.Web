782ffba72b3d9dfa43d154d76b6614fb5343125b
jithjob <jithjob@expressbase.com>
2021-06-20 13:58:35 +0530

    EbRenderQuestionsControl - initCtrls() added

--- wwwroot/js/EbControls/EbRenderQuestionsControl.js
+++ wwwroot/js/EbControls/EbRenderQuestionsControl.js
@@ -12,8 +12,38 @@
     this.DataMODEL = this.formRenderer.DataMODEL[this.ctrl.TableName];
     this.TableId = `tbl_${this.ctrl.EbSid_CtxId}`;
 
-    this.init = function () {
+    this.initCtrls = function () {
+        for (let i = 0; i < this.Questions.length; i++) {
+            let Qstn = this.Questions[i]
+            let ctrls = Qstn.ASec.Controls.$values.concat(Qstn.QSec.Controls.$values)
+            for (let i = 0; i < ctrls.length; i++) {
+                let Ctrl = ctrls[i];
 
+                let opt = {};
+                if (Ctrl.ObjType === "PowerSelect" && !Ctrl.RenderAsSimpleSelect)
+                    opt.getAllCtrlValuesFn = this.formRenderer.getWebFormVals;
+                else if (Ctrl.ObjType === "FileUploader") {
+                    opt.FormDataExtdObj = this.formRenderer.FormDataExtdObj;
+                    opt.DpControlsList = getFlatObjOfType(this.formRenderer.FormObj, "DisplayPicture");
+                }
+                else if (Ctrl.ObjType === "Date") {
+                    opt.source = "webform";
+                }
+                else if ((Ctrl.ObjType === "ExportButton") || (Ctrl.ObjType === "Phone")) {
+                    opt.formObj = this.FormObj;
+                    opt.dataRowId = this.DataMODEL[this.FormObj.TableName][0].RowId;
+                }
+
+                else if (Ctrl.ObjType === "SubmitButton") {
+                    opt.renderMode = this.renderMode;
+                }
+                this.initControls.init(Ctrl, opt);
+            }
+        }
+    };
+
+    this.init = function () {
+        this.initCtrls();
     };
 
     this.init();

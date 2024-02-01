06d2fc1da4cd0c11125d0d29a72e3fdd3afc9404
Febin Carlos <febincarlos@expressbase.com>
2024-02-01 13:12:52 +0530

    lm - ht done date removed

--- wwwroot/js/LeadManagement.js
+++ wwwroot/js/LeadManagement.js
@@ -54,7 +54,6 @@
     this.$Stars = $("#selStars");
     this.$CustomerLanguage = $("#selCustomerLanguage");
     this.$ProcedureDate = $("#txtProcedureDate");
-    this.$HtDoneDate = $("#txtHtDoneDate");
     this.$Comments = $("#txtComments");
     this.$btnNewPatient = $("#btnNewPatient");
     this.$btnNewHtEvaluation = $("#btnNewHtEvaluation");
@@ -267,7 +266,6 @@
         this.$ConsultedDate.datetimepicker({ timepicker: false, format: "d-m-Y" });
         this.$ProbableMonth.MonthPicker({ Button: this.$ProbableMonth.next().removeAttr("onclick") });
         this.$ProcedureDate.datetimepicker({ timepicker: false, format: "d-m-Y" });
-        this.$HtDoneDate.datetimepicker({ timepicker: false, format: "d-m-Y" });
 
         //FEEDBACK  BILLING  SURGERY GFC/PRP
         this.initFeedBackModal();
@@ -1057,7 +1055,6 @@
         this.$Stars.val(this.CustomerInfo["stars"]);
         this.$CustomerLanguage.val(this.CustomerInfo["cust_language"]);
         this.$ProcedureDate.val(this.CustomerInfo["procedure_date"]);
-        this.$HtDoneDate.val(this.CustomerInfo["ht_done_date"]);
         this.$Comments.val(this.CustomerInfo["comment"]);
         if (this.CustomerInfo["dprefid"] !== "0") {
             let id = this.CustomerInfo["dprefid"];
@@ -1183,7 +1180,6 @@
         this.pushToList("stars", this.$Stars.val());
         this.pushToList("cust_language", this.$CustomerLanguage.val());
         this.pushToList("procedure_date", this.$ProcedureDate.val());
-        this.pushToList("ht_done_date", this.$HtDoneDate.val());
         this.pushToList("comment", this.$Comments.val());
         this.pushToList("eb_modifiedat", this.CustomerInfo["eb_modifiedat"]);
 

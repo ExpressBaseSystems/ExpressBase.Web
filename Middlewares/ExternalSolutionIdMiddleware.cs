112649ee9909cf287cbd917b3e086b47f909e548
Jithin P V <jpeevi@hotmail.com>
2025-10-03 16:49:47 +0530

    added support for Middlewares

---
+++ Middlewares/ExternalSolutionIdMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using ExpressBase.Common;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Middlewares
+{
+    public class ExternalSolutionIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExternalSolutionIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var host = context.Request.Host.Host; 
+            string subdomain = null;
+
+            if (!string.IsNullOrEmpty(host))
+            {
+                var parts = host.Split('.');
+                if (parts.Length > 1)
+                {
+                    subdomain = parts[0];
+                }
+            }
+
+            context.Items["ExternalSolutionId"] = subdomain?.Replace(RoutingConstants.DASHDEV, string.Empty);
+
+            await _next(context);
+        }
+    }
+}
---
+++ Middlewares/TokenCheckMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using ExpressBase.Common;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ExpressBase.Web.Middlewares
+{
+    public class TokenCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            string bearerToken = context.Request.Cookies[RoutingConstants.BEARER_TOKEN] ?? "";
+            string refreshToken = context.Request.Cookies[RoutingConstants.REFRESH_TOKEN] ?? "";
+
+            if (string.IsNullOrEmpty(bearerToken) || string.IsNullOrEmpty(refreshToken))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized: Missing authentication tokens");
+                return;
+            }
+
+           
+            context.Items["BearerToken"] = bearerToken;
+            context.Items["RefreshToken"] = refreshToken;
+
+            await _next(context);
+        }
+
+    }
+}

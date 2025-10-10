73a2ec4de75b507946ecf548b628b8c2bf0b7118
Jithin P V <jpeevi@hotmail.com>
2025-10-10 13:59:30 +0530

    added app configs for various envs

---
+++ AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpressBase.Web
+{
+    public sealed class AppConfiguration
+    {
+        public string DevDomainSuffix { get; set; }
+        public string BaseHost { get; set; }
+        public string Scheme { get; set; }
+        public string LocalPort { get; set; }
+    }
+
+}

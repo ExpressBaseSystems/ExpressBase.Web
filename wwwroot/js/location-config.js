91c434658d5d40641824b49e10d4021e788ce4d2
Jithin P V <jpeevi@hotmail.com>
2025-10-17 00:59:18 +0530

    commented out  server events

--- wwwroot/js/location-config.js
+++ wwwroot/js/location-config.js
@@ -112,6 +112,7 @@
     this.imageUploader = function (container, toggle, prev, extra, viwportresize) {
         let resize = viwportresize ? true : false;
 
+        /*
         this.Cropies[extra.Name] = new EbFileUpload({       
             Type: "image",
             Toggle: toggle,
@@ -127,6 +128,7 @@
             Context: "location",//if single and crop
             ResizeViewPort: false //if single and crop
         });
+        */
 
         this.Cropies[extra.Name].uploadSuccess = function (fileid) {       
             $("#loc_logoId").val("");
